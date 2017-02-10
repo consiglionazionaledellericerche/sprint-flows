5f9db33ef72fa5158d3735fda4f1b78a3aa67250
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2017-02-10 10:14:03 +0100

    Activiti log event listener (per tracciare activiti)

--- src/main/java/it/cnr/si/flows/ng/config/ActivitiLoggingEventListener.java
+++ src/main/java/it/cnr/si/flows/ng/config/ActivitiLoggingEventListener.java
@@ -4,7 +4,9 @@ import org.activiti.engine.delegate.event.ActivitiEvent;
 import org.activiti.engine.delegate.event.ActivitiEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ActivitiLoggingEventListener implements ActivitiEventListener {
 
     private final Logger log = LoggerFactory.getLogger(ActivitiLoggingEventListener.class);
