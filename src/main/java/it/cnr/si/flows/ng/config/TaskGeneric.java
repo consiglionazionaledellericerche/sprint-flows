015bb2b7d87fbabdd47ef59fefad49285c8a856e
massimo.fraticelli <massimo.fraticelli@cnr.it>
2017-02-20 18:04:15 +0100

    ACQUISTI TRASPARENZA

---
+++ src/main/java/it/cnr/si/flows/ng/config/TaskGeneric.java
@@ -0,0 +1,27 @@
+package it.cnr.si.flows.ng.config;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.ExecutionListener;
+import org.activiti.engine.delegate.TaskListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import it.cnr.si.flows.ng.resource.FlowsProcessInstanceResource;
+
+public class TaskGeneric implements ExecutionListener {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(FlowsProcessInstanceResource.class);
+	@Override
+	public void notify(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		String nomeVariabileScelta = "sceltaUtente";
+		LOGGER.info("TaskGeneric - processId: " + execution.getId());
+		LOGGER.info("TaskGeneric - sceltaUtente: " + execution.getVariable(nomeVariabileScelta));
+		Integer currentValue = (Integer) execution.getVariable("valore");
+		Integer nextValue = new Integer(currentValue.intValue() + 1);
+		execution.setVariable("valore", nextValue);
+		LOGGER.info("valore corrente: " + nextValue);
+	}
+
+}
