1cfcda8905d1821ebe72d053a18985a71af904cf
paoloenricocirone <cironepa@gmail.com>
2017-02-24 10:48:13 +0100

    Metodo di utilità lato js che trasforma le properties mappate in "variables" in properties "normali" messe nell'array di entities

--- src/main/webapp/app/pages/details/details.controller.js
+++ src/main/webapp/app/pages/details/details.controller.js
@@ -5,9 +5,9 @@
   .module('sprintApp')
   .controller('DetailsController', DetailsController);
 
-  DetailsController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'dataService', 'AlertService', '$log'];
+  DetailsController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'dataService', 'AlertService', '$log', 'utils'];
 
-  function DetailsController ($scope, Principal, LoginService, $state, dataService, AlertService, $log) {
+  function DetailsController ($scope, Principal, LoginService, $state, dataService, AlertService, $log, utils) {
     var vm = this;
     vm.data = {};
 
@@ -18,13 +18,8 @@
         vm.data.processInstanceId = $state.params.processInstanceId;
         dataService.processInstances.byProcessInstanceId($state.params.processInstanceId).then(
                 function(response) {
-//                    todo: la stessa cosa viene fatta in tasklist.directive.js: uniformare i metodi con uno di utilità
-                    //sposto le properties dall'array variables(che cancello) in entity
-                    response.data.entity.variables.map( function(el){
-                       response.data.entity[el.name] = el.value;
-                    })
-                    delete response.data.entity.variables;
-                    vm.data = response.data;
+                    vm.data.entity = utils.refactoringVariables([response.data.entity])[0];
+                    vm.data.history = response.data.history;
                 });
     }
   }
