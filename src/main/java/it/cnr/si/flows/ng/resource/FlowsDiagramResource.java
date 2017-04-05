5014dced159165da49babca654f874f0ad1c9240
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2017-04-05 13:50:55 +0200

    grafico con sottoflussi oscurati

--- src/main/java/it/cnr/si/flows/ng/resource/FlowsDiagramResource.java
+++ src/main/java/it/cnr/si/flows/ng/resource/FlowsDiagramResource.java
@@ -1,16 +1,13 @@
 package it.cnr.si.flows.ng.resource;
 
-import com.codahale.metrics.annotation.Timed;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
 
-import it.cnr.si.flows.ng.service.FirmaService;
-
-import org.activiti.bpmn.model.Activity;
 import org.activiti.bpmn.model.BpmnModel;
 import org.activiti.bpmn.model.FlowElement;
 import org.activiti.bpmn.model.GraphicInfo;
 import org.activiti.bpmn.model.SubProcess;
-import org.activiti.bpmn.model.UserTask;
-import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
@@ -19,7 +16,6 @@ import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.Execution;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
-import org.activiti.image.ProcessDiagramGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
@@ -30,11 +26,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import com.codahale.metrics.annotation.Timed;
+
+import it.cnr.si.flows.ng.service.FlowsProcessDiagramGenerator;
 
 
 @Controller
@@ -48,12 +42,9 @@ public class FlowsDiagramResource {
     @Autowired
     private TaskService taskService;
     @Autowired
-    private ProcessDiagramGenerator pdg;
+    private FlowsProcessDiagramGenerator pdg;
     @Autowired
     private ProcessEngineConfiguration processEngineConfiguration;
-
-    @Autowired
-    private FirmaService firmaService;
 
     @RequestMapping(value = "/diagram/processDefinition/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
     @ResponseBody
@@ -87,51 +78,22 @@ public class FlowsDiagramResource {
 
         BpmnModel bpmnModel = repositoryService.getBpmnModel(processDefinition.getId());
 
-        //        org.activiti.bpmn.model.Process process = bpmnModel.getProcesses().get(0);
-        //
-        //        org.activiti.bpmn.model.Process process2 = process.clone();
-        //        Collection<FlowElement> flowElements = process2.getFlowElements();
-
         for (FlowElement fe : bpmnModel.getProcesses().get(0).getFlowElements() ) {
             GraphicInfo graphicInfo = bpmnModel.getGraphicInfo(fe.getId());
-            System.out.println(fe.getId() + fe.getName());
             if (fe instanceof SubProcess) {
-                SubProcess sp = (SubProcess) fe;
-                if (!containsActiveTasks(sp, processInstance.getId())) {
-                    graphicInfo.setExpanded(false);
-                    graphicInfo.setHeight(100);
-                    graphicInfo.setWidth(150);
-                    for (FlowElement innerFe : sp.getFlowElements()) {
-
-                        System.out.println("   "+innerFe.getId() +": "+ innerFe.getName() +" "+ innerFe.getClass().getName());
-
-                        GraphicInfo innerGraphicInfo = bpmnModel.getGraphicInfo(innerFe.getId());
-                        if (innerGraphicInfo != null) {
-                            innerGraphicInfo.setWidth(1);
-                            innerGraphicInfo.setHeight(1);
-                            innerGraphicInfo.setX(graphicInfo.getX());
-                            innerGraphicInfo.setY(graphicInfo.getY());
-                        }
-                    }
-                }
+                graphicInfo.setExpanded(containsActiveTasks((SubProcess) fe, processInstance.getId()));
             }
         };
 
         InputStream resource = pdg.generateDiagram(bpmnModel, "png", runtimeService.getActiveActivityIds(processInstance.getId()),
                 Collections.<String>emptyList(),
                 font, font, font,
-                processEngineConfiguration.getClassLoader(), 1.2);
+                processEngineConfiguration.getClassLoader(), 1.0, false);
 
         return ResponseEntity.ok(new InputStreamResource(resource));
     }
 
-    private boolean containsActiveTasks(SubProcess sp, String processInstanceId) {
-        for (FlowElement fe : sp.getFlowElements()) {
-            if (runtimeService.getActiveActivityIds(processInstanceId).contains(fe))
-                return true;
-        }
-        return false;
-    }
+
     @RequestMapping(value = "/diagram/taskInstance/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
     @ResponseBody
     @Timed
@@ -162,4 +124,12 @@ public class FlowsDiagramResource {
 
     }
 
+
+    private boolean containsActiveTasks(SubProcess sp, String processInstanceId) {
+        for (FlowElement fe : sp.getFlowElements()) {
+            if (runtimeService.getActiveActivityIds(processInstanceId).contains(fe.getId()))
+                return true;
+        }
+        return false;
+    }
 }
