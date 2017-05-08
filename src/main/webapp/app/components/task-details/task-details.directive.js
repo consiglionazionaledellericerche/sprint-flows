a15d57c9dc8fc1330d51839d37e4002c03cae5ad
paoloenricocirone <cironepa@gmail.com>
2017-05-08 11:43:14 +0200

    History del task ( Issue #14):   -  Migliorie dei campi visualizzati   -  Aggiunta funzionalità "dettagli" per ogni task dell'history

--- src/main/webapp/app/components/task-details/task-details.directive.js
+++ src/main/webapp/app/components/task-details/task-details.directive.js
@@ -2,44 +2,33 @@
   'use strict';
 
   angular.module('sprintApp')
-  .directive('task-details', taskDetailsDirective);
+  .directive('taskDetails', taskDetailsDirective);
 
-  taskDetailsDirective.$inject = ['dataService', '$sessionStorage', '$log'];
+  taskDetailsDirective.$inject = ['dataService', '$sessionStorage', '$uibModal', '$log'];
 
-  function taskDetailsDirective(dataService, $sessionStorage, $log) {
+  function taskDetailsDirective(dataService, $sessionStorage, $uibModal, $log) {
 
     return {
       restrict: 'E',
       scope: {
-        tasks: '=',
-        paging: '=',
-        advanced: '=',
-        detailed: '=',
-        selectProcessDefinitionKey: '=',
-        processDefinitions: '='
+        task: '='
       },
-      templateUrl: 'app/components/task-details/task.details.html',
-      link: function (scope, element, attrs) {
+      templateUrl: 'app/components/task-details/task-details.html',
+      link: function (scope) {
 
         scope.modalTaskMetadata = function (task) {
           $log.info(task);
-//          todo: implementare il sercice
-          taskService.getTaskMetadata(task.name.replace(':', '_')).then(function (data) {
-            var displayData = "", columnValue;
-            _.each(data.fields, function (item) {
-              try {
-                columnValue = task.properties[item.property.replace(':', '_')];
-                if (columnValue !== undefined) {
-                  displayData += "<p><b>" + i18nService.i18n(item.label) + "</b> " + columnValue + "</p>";
+            $uibModal.open({
+                templateUrl: 'app/components/task-details/detailModal.html',
+                controllerAs: 'vm',
+                controller: 'DetailModalController',
+                size: 'md',
+                resolve: {
+                    task: function() {
+                        return task;
+                    }
                 }
-              } catch (err) {
-                $log.info(err);
-              }
             });
-            $log.info(displayData);
-            var modalContent = modalService.modal("Dettaglio task: " + task.title, displayData);
-            $('<div class="modal fade role="dialog" tabindex="-1"></div>').append(modalContent).modal();
-          });
         };
       }
     };
