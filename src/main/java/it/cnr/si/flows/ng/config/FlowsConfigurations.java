74c3b9ea6f1231271de4f479b0b926ac3780c2a7
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2017-05-30 13:29:58 +0200

    Fonts

--- src/main/java/it/cnr/si/flows/ng/config/FlowsConfigurations.java
+++ src/main/java/it/cnr/si/flows/ng/config/FlowsConfigurations.java
@@ -78,21 +78,21 @@ public class FlowsConfigurations {
         beans.put("startAcquistiSetGroupsPrototipoSenzaAce", getStartAcquistiSetGroupsPrototipoSenzaAce());
         conf.setBeans(beans);
 
-        // configurare il font in cnr.activiti.diagram-font, solo se e' installato
-//        if ( Arrays.asList(java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames())
-//                .stream()
-//                .anyMatch(f -> f.equals(diagramFont))) {
-//            LOGGER.info("Font {} trovato, imposto per i diagrammi", diagramFont);
-//            try {
-//                conf.setActivityFontName(diagramFont);
-//                conf.setAnnotationFontName(diagramFont);
-//                conf.setLabelFontName(diagramFont);
-//            } catch (Exception e) {
-//                LOGGER.warn("Settaggio del Font {} ha dato errore", diagramFont);
-//            }
-//        } else {
-//            LOGGER.warn("Font {} non trovato, torno al default", diagramFont);
-//        }
+        //         configurare il font in cnr.activiti.diagram-font, solo se e' installato
+        if ( Arrays.asList(java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames())
+                .stream()
+                .anyMatch(f -> f.equals(diagramFont))) {
+            LOGGER.info("Font {} trovato, imposto per i diagrammi", diagramFont);
+            try {
+                conf.setActivityFontName(diagramFont);
+                conf.setAnnotationFontName(diagramFont);
+                conf.setLabelFontName(diagramFont);
+            } catch (Exception e) {
+                LOGGER.warn("Settaggio del Font {} ha dato errore", diagramFont);
+            }
+        } else {
+            LOGGER.warn("Font {} non trovato, torno al default", diagramFont);
+        }
 
         // async migliora le prestazioni, in particolare con tanti utenti
         conf.setAsyncExecutorActivate(true);
