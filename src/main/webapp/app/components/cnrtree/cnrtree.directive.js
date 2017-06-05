4010864c4f29f6a30d5c76618f649f0365ca215a
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2017-06-05 16:19:57 +0200

    coerenza di codice

--- src/main/webapp/app/components/cnrtree/cnrtree.directive.js
+++ src/main/webapp/app/components/cnrtree/cnrtree.directive.js
@@ -19,31 +19,31 @@
             templateUrl: 'app/components/cnrtree/cnrtree.html',
 
 
-            link: function link(scope) {
+            link: function link($scope) {
 
-                scope.treeConfig = {
+                $scope.treeConfig = {
                         version: 1
                 };
-                scope.jsonlist = [];
-                scope.ignoreModelChanges = function() {return true;};
+                $scope.jsonlist = [];
+                $scope.ignoreModelChanges = function() {return true;};
 
-                dataService.dynamiclist.byName(scope.listName).then(
+                dataService.dynamiclist.byName($scope.listName).then(
                         function(response) {
                             var lists = JSON.parse(response.data.listjson);
-                            var type = (scope.cdsuo !== undefined && scope.cdsuo !== '') ? scope.cdsuo : 'default';
-                            scope.jsonlist = lists[type];
-                            scope.treeConfig.version++;
+                            var type = ($scope.cdsuo !== undefined && $scope.cdsuo !== '') ? $scope.cdsuo : 'default';
+                            $scope.jsonlist = lists[type];
+                            $scope.treeConfig.version++;
                         }, function(response) {
                             $log.error(response);
                         }
                 );
 
-                scope.select_node = function (discard, selection) {
+                $scope.select_node = function (discard, selection) {
                     if (selection.node.children.length === 0) {
-                        scope.ngModel = selection.node.text;
-                        scope.ngModelid = selection.node.id;
+                        $scope.ngModel = selection.node.text;
+                        $scope.ngModelid = selection.node.id;
                     } else {
-                        scope.treeInstance.jstree(true).deselect_all({});
+                        $scope.treeInstance.jstree(true).deselect_all({});
                     }
                 };
             }
