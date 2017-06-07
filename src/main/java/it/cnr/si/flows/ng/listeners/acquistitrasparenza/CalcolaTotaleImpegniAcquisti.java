d2616207f095aeb5e234c11292bfd3512665677a
massimo.fraticelli <massimo.fraticelli@cnr.it>
2017-06-07 14:28:33 +0200

    calcolo totale impegni

---
+++ src/main/java/it/cnr/si/flows/ng/listeners/acquistitrasparenza/CalcolaTotaleImpegniAcquisti.java
@@ -0,0 +1,35 @@
+package it.cnr.si.flows.ng.listeners.acquistitrasparenza;
+
+import java.util.List;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class CalcolaTotaleImpegniAcquisti implements ExecutionListener {
+	private static final long serialVersionUID = 686169707042367215L;
+	private static final Logger LOGGER = LoggerFactory.getLogger(CalcolaTotaleImpegniAcquisti.class);
+
+	@Override
+	public void notify(DelegateExecution execution) throws Exception {
+
+		double importoTotale = 0.0;	
+		for(int i = 0; i<=100; i=i+1) {
+			String Elemento = "impegni[" +  i + "][importo]";
+			if(execution.getVariable(Elemento) != null){
+				double importo = Double.parseDouble((String) execution.getVariable(Elemento));
+				importoTotale = importoTotale + importo;
+			} else {
+				break;
+			}
+			execution.setVariable("importoTotale", importoTotale);
+		}
+
+	}
+}
