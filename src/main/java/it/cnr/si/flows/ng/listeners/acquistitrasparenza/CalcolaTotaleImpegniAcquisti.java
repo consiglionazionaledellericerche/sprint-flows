d3d84ea1e49861a6e83f5248ab1817fea4caa91f
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2017-06-16 10:26:01 +0200

    Calcola Totale Impegni e ulizia listeners

--- src/main/java/it/cnr/si/flows/ng/listeners/acquistitrasparenza/CalcolaTotaleImpegniAcquisti.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/acquistitrasparenza/CalcolaTotaleImpegniAcquisti.java
@@ -1,42 +1,42 @@
 package it.cnr.si.flows.ng.listeners.acquistitrasparenza;
 
-import java.util.List;
-
 import org.activiti.engine.delegate.BpmnError;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.ExecutionListener;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Component;
 
 
 
 @Component
 public class CalcolaTotaleImpegniAcquisti implements ExecutionListener {
-	private static final long serialVersionUID = 686169707042367215L;
-	private static final Logger LOGGER = LoggerFactory.getLogger(CalcolaTotaleImpegniAcquisti.class);
+    private static final long serialVersionUID = 686169707042367215L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CalcolaTotaleImpegniAcquisti.class);
 
-	@Override
-	public void notify(DelegateExecution execution) throws Exception {
-	    throw new BpmnError("500", "Questo Listener va rifattorizzato con il nuovo JSON degli impegni");
 
-//		double importoTotale = 0.0;
-//		for(int i = 0; i<=100; i=i+1) {
-//			String Elemento = "impegni[" +  i + "][importo]";
-//			if(execution.getVariable(Elemento) != null){
-//				String importoImpegnoSingolo = (String) execution.getVariable(Elemento);
-//				try {
-//					double importo = Double.parseDouble(importoImpegnoSingolo);
-//					importoTotale = importoTotale + importo;
-//				} catch (NumberFormatException e ){
-//					LOGGER.error("Formato Impegno Non Valido {} nel flusso {} - {}", importoImpegnoSingolo, execution.getId(), execution.getVariable("title"));
-//					throw new BpmnError("400", "Formato Impegno Non Valido: " + importoImpegnoSingolo);
-//				}
-//			} else {
-//				break;
-//			}
-//			execution.setVariable("importoTotale", importoTotale);
-//		}
-	}
+    @Override
+    public void notify(DelegateExecution execution) throws Exception {
+
+        double importoTotale = 0.0;
+
+        String impegniString = (String) execution.getVariable("impegniVeri");
+        JSONArray impegni = new JSONArray(impegniString);
+
+        for ( int i = 0; i < impegni.length(); i++) {
+
+            JSONObject impegno = impegni.getJSONObject(i);
+            try {
+                importoTotale += impegno.getDouble("importo");
+            } catch (JSONException e) {
+                LOGGER.error("Formato Impegno Non Valido {} nel flusso {} - {}", impegno.getString("importo"), execution.getId(), execution.getVariable("title"));
+                throw new BpmnError("400", "Formato Impegno Non Valido: " + impegno.getString("importo"));
+            }
+        }
+
+        execution.setVariable("importoTotale", importoTotale);
+    }
 }
