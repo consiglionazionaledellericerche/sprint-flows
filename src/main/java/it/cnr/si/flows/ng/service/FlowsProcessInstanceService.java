01990530acd2f5c9eaeb0a550f1c6e8bec45cbd2
paoloenricocirone <cironepa@gmail.com>
2017-06-16 16:41:44 +0200

    refactoring funzionalità search: creazione metodo nel service

--- src/main/java/it/cnr/si/flows/ng/service/FlowsProcessInstanceService.java
+++ src/main/java/it/cnr/si/flows/ng/service/FlowsProcessInstanceService.java
@@ -6,20 +6,29 @@ import org.activiti.engine.RepositoryService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.history.HistoricIdentityLink;
 import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.history.HistoricProcessInstanceQuery;
 import org.activiti.engine.impl.RepositoryServiceImpl;
 import org.activiti.engine.impl.pvm.PvmActivity;
 import org.activiti.engine.impl.pvm.ReadOnlyProcessDefinition;
 import org.activiti.engine.impl.task.TaskDefinition;
+import org.activiti.engine.impl.util.json.JSONArray;
+import org.activiti.engine.impl.util.json.JSONObject;
 import org.activiti.engine.task.IdentityLink;
 import org.activiti.rest.service.api.RestResponseFactory;
+import org.activiti.rest.service.api.history.HistoricProcessInstanceResponse;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static it.cnr.si.flows.ng.utils.Utils.*;
 
 /**
  * Created by cirone on 15/06/17.
@@ -27,6 +36,7 @@ import java.util.Map;
 @Service
 public class FlowsProcessInstanceService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlowsProcessInstanceService.class);
     @Inject
     FlowsAttachmentService flowsAttachmentService;
     @Inject
@@ -37,6 +47,7 @@ public class FlowsProcessInstanceService {
     private RepositoryService repositoryService;
     @Inject
     private TaskService taskService;
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
 
     public Map<String, Object> getProcessInstanceWithDetails(@RequestParam("processInstanceId") String processInstanceId) {
@@ -88,5 +99,78 @@ public class FlowsProcessInstanceService {
         result.put("history", history);
         return result;
     }
+
+    public Map<String, Object> search(HttpServletRequest req, String processInstanceId, boolean active, String order, int firstResult, int maxResults) {
+        String jsonString = "";
+        Map<String, Object> result = new HashMap<>();
+
+        try {
+            jsonString = IOUtils.toString(req.getReader());
+        } catch (Exception e) {
+            LOGGER.error("Errore nella letture dello stream della request", e);
+        }
+        JSONArray params = new JSONObject(jsonString).getJSONArray("params");
+
+        HistoricProcessInstanceQuery processQuery = historyService.createHistoricProcessInstanceQuery();
+
+        if (!processInstanceId.equals(ALL_PROCESS_INSTANCES))
+            processQuery.processDefinitionKey(processInstanceId);
+
+        if (active)
+            processQuery.unfinished();
+        else
+            processQuery.finished();
+
+        for (int i = 0; i < params.length(); i++) {
+            JSONObject appo = params.optJSONObject(i);
+            String key = appo.getString("key");
+            String value = appo.getString("value");
+            String type = appo.getString("type");
+            //wildcard ("%") di default ma non a TUTTI i campi
+            switch (type) {
+                case "textEqual":
+                    processQuery.variableValueEquals(key, value);
+                    break;
+                case "boolean":
+                    // gestione variabili booleane
+                    processQuery.variableValueEquals(key, Boolean.valueOf(value));
+                    break;
+                case "date":
+                    processDate(processQuery, key, value);
+                    break;
+                default:
+                    //variabili con la wildcard  (%value%)
+                    processQuery.variableValueLikeIgnoreCase(key, "%" + value + "%");
+                    break;
+            }
+        }
+        if (order.equals(ASC))
+            processQuery.orderByProcessInstanceStartTime().asc();
+        else if (order.equals(DESC))
+            processQuery.orderByProcessInstanceStartTime().desc();
+
+        long totalItems = processQuery.includeProcessVariables().count();
+        result.put("totalItems", totalItems);
+
+        List<HistoricProcessInstance> taskRaw = processQuery.includeProcessVariables().listPage(firstResult, maxResults);
+        List<HistoricProcessInstanceResponse> tasks = restResponseFactory.createHistoricProcessInstanceResponseList(taskRaw);
+        result.put("processInstances", tasks);
+        return result;
+    }
+
+
+    private void processDate(HistoricProcessInstanceQuery taskQuery, String key, String value) {
+        try {
+            Date date = sdf.parse(value);
+
+            if (key.contains("Less")) {
+                taskQuery.variableValueLessThanOrEqual(key.replace("Less", ""), date);
+            } else if (key.contains("Great"))
+                taskQuery.variableValueGreaterThanOrEqual(key.replace("Great", ""), date);
+        } catch (ParseException e) {
+            LOGGER.error("Errore nel parsing della data {} - ", value, e);
+        }
+    }
+
 
 }
