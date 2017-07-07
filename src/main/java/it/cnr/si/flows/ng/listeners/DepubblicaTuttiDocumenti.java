4416611adf6dae6c962fc8c7d6f4e54e139b35ed
massimo.fraticelli <massimo.fraticelli@cnr.it>
2017-07-07 10:16:48 +0200

    depubblica tutti i documenti in caso di revoca

---
+++ src/main/java/it/cnr/si/flows/ng/listeners/DepubblicaTuttiDocumenti.java
@@ -0,0 +1,43 @@
+package it.cnr.si.flows.ng.listeners;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.activiti.engine.delegate.BpmnError;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import it.cnr.si.flows.ng.service.FlowsAttachmentService;
+
+@Component
+public class DepubblicaTuttiDocumenti implements ExecutionListener {
+
+	private static final long serialVersionUID = -56001764662303256L;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DepubblicaTuttiDocumenti.class);
+
+	@Inject
+	private FlowsAttachmentService attachmentService;
+
+
+	@Override
+	public void notify(DelegateExecution execution) throws Exception {
+
+
+		Map<String, Object> mappaVariabili = execution.getVariables();
+
+		for (Map.Entry<String, Object> entry : mappaVariabili.entrySet()) {
+			if (entry.getValue() != null){
+				if (entry.getValue().toString().startsWith("it.cnr.si.flows.ng.dto.FlowsAttachment@")){
+					LOGGER.info("depubblico: " + entry.getKey() + " per il flusso: " + execution.getId() );
+					attachmentService.setPubblicabile(execution.getId(), entry.getKey(), false);
+				}				
+			}				
+		}
+	}
+}
