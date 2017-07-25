624a031f9bac203e736d45230f5504ab6c27e9dc
paoloenricocirone <cironepa@gmail.com>
2017-07-25 16:12:49 +0200

    Migliorie della prestazione della pagina "dettagli": ora viene evitata la chiamata al service che restituisce i documenti associati alla Process Instances perchè sono già presenti nella query con i dati della Process Instances

--- src/main/webapp/app/pages/details/details.controller.js
+++ src/main/webapp/app/pages/details/details.controller.js
@@ -13,6 +13,8 @@
         vm.taskId = $state.params.taskId;
         $scope.processInstanceId = $state.params.processInstanceId; // mi torna comodo per gli attachments -martin
 
+
+
         if ($state.params.processInstanceId) {
             dataService.processInstances.byProcessInstanceId($state.params.processInstanceId).then(
                 function(response) {
@@ -29,45 +31,59 @@
                     vm.detailsView = 'api/views/' + processDefinitionKey + '/detail';
                     vm.data.history.forEach(function(el) {
                         //recupero l'ultimo task (quello ancora da eseguire)
-                        if (el.historyTask.endTime === null){
+                        if (el.historyTask.endTime === null) {
                             //recupero la fase
                             vm.data.fase = el.historyTask.name;
                             //recupero il gruppo/l'utente assegnatario del task
-                            el.historyIdentityLink.forEach(function(il){
-                              if(il.type === "candidate")
-                                if(il.groupId !== null)
-                            		vm.data.groupCandidate = il.groupId;
-                            	else
-                            	    vm.data.userCandidate = il.userId
+                            el.historyIdentityLink.forEach(function(il) {
+                                if (il.type === "candidate")
+                                    if (il.groupId !== null)
+                                        vm.data.groupCandidate = il.groupId;
+                                    else
+                                        vm.data.userCandidate = il.userId
                             })
                         }
                     });
                 });
         }
 
+
         $scope.exportSummary = function(url, filename) {
             utils.downloadFile(url, filename, 'application/pdf');
         };
 
-        $scope.editVariable = function(variableName, currentValue) {
-          $uibModal.open({
-            templateUrl: 'app/pages/details/editvariable.modal.html',
-            controller: 'EditVariableModalController',
-            controllerAs: 'vm',
-            size: 'md',
-            resolve: {
-              processInstanceId: function() {
-                    return $scope.processInstanceId;
-                },
-                variableName: function() {
-                    return variableName;
-                },
-                currentValue: function() {
-                    return currentValue;
-                }
 
+        $scope.parse = function(attachments) {
+            if (attachments) {
+                var appo = [];
+                for (var attachment in attachments) {
+                    delete attachments[attachment].bytes;
+                    appo.push(attachments[attachment]);
+                }
+                return appo;
             }
-          })
+        }
+
+
+        $scope.editVariable = function(variableName, currentValue) {
+            $uibModal.open({
+                templateUrl: 'app/pages/details/editvariable.modal.html',
+                controller: 'EditVariableModalController',
+                controllerAs: 'vm',
+                size: 'md',
+                resolve: {
+                    processInstanceId: function() {
+                        return $scope.processInstanceId;
+                    },
+                    variableName: function() {
+                        return variableName;
+                    },
+                    currentValue: function() {
+                        return currentValue;
+                    }
+
+                }
+            })
         }
     }
 })();
