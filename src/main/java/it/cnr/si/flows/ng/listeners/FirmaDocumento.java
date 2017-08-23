93dec8dc1c21770dc1900501a76d3560c12baf6e
massimo.fraticelli <massimo.fraticelli@cnr.it>
2017-08-23 16:25:38 +0200

    discriminzazione del messaggio di errore nella firma a secondo del codice di errore di Aruba

--- src/main/java/it/cnr/si/flows/ng/listeners/FirmaDocumento.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/FirmaDocumento.java
@@ -1,5 +1,6 @@
 package it.cnr.si.flows.ng.listeners;
 
+import org.activiti.engine.delegate.BpmnError;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.ExecutionListener;
 import org.activiti.engine.delegate.Expression;
@@ -17,57 +18,67 @@ import it.cnr.si.flows.ng.service.FlowsAttachmentService;
 @Component
 public class FirmaDocumento implements ExecutionListener {
 
-    private static final long serialVersionUID = -56001764662303256L;
+	private static final long serialVersionUID = -56001764662303256L;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FirmaDocumento.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(FirmaDocumento.class);
 
-    @Autowired
-    private FlowsFirmaService firmaService;
-    @Autowired
-    private FlowsAttachmentService attachmentService;
-
-
-    private Expression nomeFileDaFirmare;
-
-    @Override
-    public void notify(DelegateExecution execution) throws Exception {
-        if (!execution.getEventName().equals(ExecutionListener.EVENTNAME_TAKE))
-            throw new IllegalStateException("Questo Listener accetta solo eventi 'take'.");
-        if (nomeFileDaFirmare.getValue(execution) == null)
-            throw new IllegalStateException("Questo Listener ha bisogno del campo 'nomeFileDaFirmare' nella process definition (nel Task Listener - Fields).");
-
-        String nomeVariabileFile = (String) nomeFileDaFirmare.getValue(execution);
-
-        // TODO: validare presenza di queste tre variabili
-        String username = (String) execution.getVariable("username");
-        String password = (String) execution.getVariable("password");
-        String otp = (String) execution.getVariable("otp");
-
-        FlowsAttachment att = (FlowsAttachment) execution.getVariable(nomeVariabileFile);
-        byte[] bytes = att.getBytes();
-
-        try {
-            byte[] bytesfirmati = firmaService.firma(username, password, otp, bytes);
-            att.setBytes(bytesfirmati);
-            att.setFilename(getSignedFilename(att.getFilename()));
-            att.setAzione(FlowsAttachment.Azione.Firma);
-            att.addStato(FlowsAttachment.Stato.Firmato);
-
-            attachmentService.saveAttachment(execution, nomeVariabileFile, att);
-
-        } catch (ArubaSignServiceException e) {
-            LOGGER.error("firma non riuscita", e);
-            throw new TaskFailedException(e);
-        }
-
-    }
+	@Autowired
+	private FlowsFirmaService firmaService;
+	@Autowired
+	private FlowsAttachmentService attachmentService;
 
 
-    private static String getSignedFilename(String filename) {
-        String result = filename.substring(0, filename.lastIndexOf('.'));
-        result += ".signed";
-        result += filename.substring(filename.lastIndexOf('.'));
-        return result;
-    }
+	private Expression nomeFileDaFirmare;
+
+	@Override
+	public void notify(DelegateExecution execution) throws Exception {
+		if (!execution.getEventName().equals(ExecutionListener.EVENTNAME_TAKE))
+			throw new IllegalStateException("Questo Listener accetta solo eventi 'take'.");
+		if (nomeFileDaFirmare.getValue(execution) == null)
+			throw new IllegalStateException("Questo Listener ha bisogno del campo 'nomeFileDaFirmare' nella process definition (nel Task Listener - Fields).");
+
+		String nomeVariabileFile = (String) nomeFileDaFirmare.getValue(execution);
+
+		// TODO: validare presenza di queste tre variabili
+		String username = (String) execution.getVariable("username");
+		String password = (String) execution.getVariable("password");
+		String otp = (String) execution.getVariable("otp");
+		String textMessage = "";
+
+		FlowsAttachment att = (FlowsAttachment) execution.getVariable(nomeVariabileFile);
+		byte[] bytes = att.getBytes();
+
+		try {
+			byte[] bytesfirmati = firmaService.firma(username, password, otp, bytes);
+			att.setBytes(bytesfirmati);
+			att.setFilename(getSignedFilename(att.getFilename()));
+			att.setAzione(FlowsAttachment.Azione.Firma);
+			att.addStato(FlowsAttachment.Stato.Firmato);
+
+			attachmentService.saveAttachment(execution, nomeVariabileFile, att);
+
+		} catch (ArubaSignServiceException e) {
+			LOGGER.error("firma non riuscita", e);
+			if (e.getMessage().indexOf("error code 0001") != -1) {
+				textMessage = "controlla il formato del file sottopsto alla firma";
+			} else if(e.getMessage().indexOf("error code 0003") != -1) {
+				textMessage = "Errore in fase di verifica delle credenziali";
+			} else if(e.getMessage().indexOf("error code 0004") != -1) {
+				textMessage = "Errore nel PIN";
+			} else {
+				textMessage = "errore generico";
+			}
+			throw new BpmnError("500", "firma non riuscita - " + textMessage);
+		}
+
+	}
+
+
+	private static String getSignedFilename(String filename) {
+		String result = filename.substring(0, filename.lastIndexOf('.'));
+		result += ".signed";
+		result += filename.substring(filename.lastIndexOf('.'));
+		return result;
+	}
 
 }
