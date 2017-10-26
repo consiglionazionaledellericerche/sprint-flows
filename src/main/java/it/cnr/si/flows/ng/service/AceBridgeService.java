ae41610c60d9cc1955c1bc6ab302d34a6ce8703f
paoloenricocirone <cironepa@gmail.com>
2017-10-26 11:46:52 +0200

    Commit refactoring e rimozione codice duplicato in AceBridgeService.java

--- src/main/java/it/cnr/si/flows/ng/service/AceBridgeService.java
+++ src/main/java/it/cnr/si/flows/ng/service/AceBridgeService.java
@@ -1,18 +1,17 @@
 package it.cnr.si.flows.ng.service;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AceBridgeService {
@@ -20,14 +19,14 @@ public class AceBridgeService {
 	@Resource(name = "aceJdbcTemplate")
 	private JdbcTemplate aceJdbcTemplate;
 
-	private static final String groupsForUser = "SELECT persona.nome, persona.cognome, persona.userid, persona.id, ruolo.sigla, ruolo.descr, ruolo.id,entitaorganizzativa.sigla as eosigla, entitaorganizzativa.denominazione, entitaorganizzativa.id as eoid "+
+    private static final String GROUPS_FOR_USER = "SELECT persona.nome, persona.cognome, persona.userid, persona.id, ruolo.sigla, ruolo.descr, ruolo.id,entitaorganizzativa.sigla as eosigla, entitaorganizzativa.denominazione, entitaorganizzativa.id as eoid " +
 			"FROM ace.assegnazioneruolo "+
 			"INNER JOIN ace.persona ON persona.id = assegnazioneruolo.ass_persona_id "+
 			"INNER JOIN ace.ruolo ON ruolo.id = assegnazioneruolo.ruolo_id "+
 			"INNER JOIN ace.entitaorganizzativa ON entitaorganizzativa.id = assegnazioneruolo.entitaorganizzativa_id "+
 			"where assegnazioneruolo.ass_persona_id = (SELECT id FROM ace.persona WHERE persona.userid = ?)";
 
-	private static final String usersInRole = "SELECT persona.nome, persona.cognome, persona.id, persona.userid, ruolo.sigla, ruolo.descr, ruolo.id,entitaorganizzativa.sigla as eosigla, entitaorganizzativa.denominazione, entitaorganizzativa.id as eoid "+
+    private static final String USERS_IN_ROLE = "SELECT persona.nome, persona.cognome, persona.id, persona.userid, ruolo.sigla, ruolo.descr, ruolo.id,entitaorganizzativa.sigla as eosigla, entitaorganizzativa.denominazione, entitaorganizzativa.id as eoid " +
 			"FROM ace.assegnazioneruolo "+
 			"INNER JOIN ace.persona ON persona.id = assegnazioneruolo.ass_persona_id "+
 			"INNER JOIN ace.ruolo ON ruolo.id = assegnazioneruolo.ruolo_id "+
@@ -35,7 +34,7 @@ public class AceBridgeService {
 			"where ruolo.sigla = ?  "+
 			"and entitaorganizzativa.id = ?";
 
-	private static final String uoLike = "select distinct entitaorganizzativa.id, entitaorganizzativa.sigla, entitaorganizzativa.denominazione, entitaorganizzativa.cdsuo " +
+    private static final String UO_LIKE = "select distinct entitaorganizzativa.id, entitaorganizzativa.sigla, entitaorganizzativa.denominazione, entitaorganizzativa.cdsuo " +
 			"from ace.entitaorganizzativa " +
 			"INNER JOIN ace.tipoentitaorganizzativa ON tipoentitaorganizzativa.id = entitaorganizzativa.tipo_id " +
 			"where  " +
@@ -49,21 +48,21 @@ public class AceBridgeService {
 			"AND (entitaorganizzativa.finevalidita IS NULL AND entitaorganizzativa.cdsuo <> 'SOPPRE') " +
 			"AND (entitaorganizzativa.sigla ilike ? OR entitaorganizzativa.denominazione ilike ?)";
 
-	private static final String denominazioneStruttura = "Select entitaorganizzativa.denominazione, entitaorganizzativa.sigla, entitaorganizzativa.denominazionebreve "
+    private static final String DENOMINAZIONE_STRUTTURA = "Select entitaorganizzativa.denominazione, entitaorganizzativa.sigla, entitaorganizzativa.denominazionebreve "
 			+ "from ace.entitaorganizzativa "
 			+ "where entitaorganizzativa.id = ?";
 
-	private static final String denominazioneRuolo = "Select ruolo.descr, ruolo.sigla, ruolo.id "
+    private static final String DENOMINAZIONE_RUOLO = "Select ruolo.descr, ruolo.sigla, ruolo.id "
 			+ "from ace.ruolo "
 			+ "where ruolo.sigla = ?";
 
 
 	public List<String> getAceGroupsForUser(String username) {
 
-		return aceJdbcTemplate.query(groupsForUser, new Object[] {username}, new RowMapper<String>() {
+        return aceJdbcTemplate.query(GROUPS_FOR_USER, new Object[]{username}, new RowMapper<String>() {
 			@Override
 			public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-					return rs.getString("sigla") +"@"+ rs.getString("eoid");
+                return rs.getString("sigla") + "@" + rs.getString("eoid");
 			}
 		});
 	}
@@ -71,17 +70,16 @@ public class AceBridgeService {
 	public List<String> getUsersinAceGroup(String groupName) {
 
 		if (!groupName.contains("@"))
-			return null;
+            return new ArrayList<>();
 
 		String[] split = groupName.split("@");
 		String sigla = split[0];
 		Integer eo = Integer.parseInt(split[1]);
 		Object[] args = new Object[] {sigla, eo};
 
-		return aceJdbcTemplate.query(usersInRole, args, new RowMapper<String>() {
+        return aceJdbcTemplate.query(USERS_IN_ROLE, args, new RowMapper<String>() {
 			@Override
 			public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-				//return rs.getString("sigla") +"@"+ rs.getString("eoid");
 				return rs.getString("userid");
 			}
 		});
@@ -91,7 +89,7 @@ public class AceBridgeService {
 		uoName = "%" + uoName + "%";
 		Object[] args = new Object[] {uoName, uoName};
 
-		return aceJdbcTemplate.query(uoLike, args, new RowMapper<Pair<Integer, String>>() {
+        return aceJdbcTemplate.query(UO_LIKE, args, new RowMapper<Pair<Integer, String>>() {
 			@Override
 			public Pair<Integer, String> mapRow(ResultSet rs, int rowNum) throws SQLException {
 				Integer idUo = rs.getInt("id");
@@ -109,8 +107,8 @@ public class AceBridgeService {
 
 	@Cacheable("nomiStrutture")
 	public String getNomeStruturaById(Integer id) {
-		return aceJdbcTemplate.query(denominazioneStruttura, new Object[] {id}, new ResultSetExtractor<String>() {
-			public String extractData(ResultSet rs) throws SQLException, DataAccessException {
+        return aceJdbcTemplate.query(DENOMINAZIONE_STRUTTURA, new Object[]{id}, new ResultSetExtractor<String>() {
+            public String extractData(ResultSet rs) throws SQLException {
 				rs.next();
 				return rs.getString("denominazione");
 			}
@@ -126,14 +124,14 @@ public class AceBridgeService {
 		String ruoloName = splitGroupRuoloStrutturaName[0];
 		Integer strutturaId = Integer.parseInt(splitGroupRuoloStrutturaName[1]) ;
 
-		String descrizioneRuolo = aceJdbcTemplate.query(denominazioneRuolo, new Object[] {ruoloName}, new ResultSetExtractor<String>() {
-			public String extractData(ResultSet rs) throws SQLException, DataAccessException {
+        String descrizioneRuolo = aceJdbcTemplate.query(DENOMINAZIONE_RUOLO, new Object[]{ruoloName}, new ResultSetExtractor<String>() {
+            public String extractData(ResultSet rs) throws SQLException {
 				rs.next();
 				return rs.getString("descr");
 			}
 		});
-		String descrizioneStruttura = aceJdbcTemplate.query(denominazioneStruttura, new Object[] {strutturaId}, new ResultSetExtractor<String>() {
-			public String extractData(ResultSet rs) throws SQLException, DataAccessException {
+        String descrizioneStruttura = aceJdbcTemplate.query(DENOMINAZIONE_STRUTTURA, new Object[]{strutturaId}, new ResultSetExtractor<String>() {
+            public String extractData(ResultSet rs) throws SQLException {
 				rs.next();
 				return rs.getString("sigla");
 			}
