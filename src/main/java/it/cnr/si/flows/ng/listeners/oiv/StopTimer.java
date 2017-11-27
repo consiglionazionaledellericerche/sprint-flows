f8e9175456d9bebfb893a8b0eaf8452a447aa07e
paoloenricocirone <cironepa@gmail.com>
2017-11-27 18:08:13 +0100

    estrazione nome timer

--- src/main/java/it/cnr/si/flows/ng/listeners/oiv/StopTimer.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/oiv/StopTimer.java
@@ -1,24 +1,16 @@
 package it.cnr.si.flows.ng.listeners.oiv;
 
-import java.util.List;
-
-import org.activiti.engine.delegate.BpmnError;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.ExecutionListener;
-import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
-import org.activiti.engine.impl.jobexecutor.JobExecutor;
-import org.activiti.engine.impl.jobexecutor.JobHandler;
+import org.activiti.engine.impl.persistence.entity.TimerEntity;
 import org.activiti.engine.runtime.Execution;
 import org.activiti.engine.runtime.Job;
 import org.activiti.engine.runtime.JobQuery;
-import org.activiti.engine.runtime.ProcessInstance;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.activiti.engine.impl.jobexecutor.TimerDeclarationImpl;
+
+import java.util.List;
 
 
 
@@ -41,10 +33,13 @@ public class StopTimer implements ExecutionListener {
         {
         	Execution jobExecution = execution.getEngineServices().getRuntimeService().createExecutionQuery()
        	 	.executionId(job.getExecutionId()).singleResult();
-        	String timerDeclarationImplVal = ((TimerDeclarationImpl)job).getJobHandlerConfiguration().toString();
-            LOGGER.debug("getDuedate {}, getId {}, TimerDeclarationImpl {}", job.getDuedate(), job.getId(), timerDeclarationImplVal);
-            if (timerDeclarationImplVal.contains("boundarytimer4")) {
-                LOGGER.debug("--- trovatogetDuedate {}, getId {}, timerDeclarationImplVal{}", job.getDuedate(), job.getId(), timerDeclarationImplVal);
+            String timerName = ((TimerEntity) job).getJobHandlerConfiguration()
+                    .split(":")[1]
+                    .replace("\"", "")
+                    .replace("}", "");
+            LOGGER.debug("getDuedate {}, getId {}, TimerDeclarationImpl {}", job.getDuedate(), job.getId(), timerName);
+            if (timerName.equals("boundarytimer4")) {
+                LOGGER.debug("--- trovatogetDuedate {}, getId {}, timerName{}", job.getDuedate(), job.getId(), timerName);
             	job.wait();
             }
         }
