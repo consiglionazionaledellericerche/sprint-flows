fa893a3a5bdfdd2561c410a81041ec7b51e68d54
paoloenricocirone <cironepa@gmail.com>
2017-12-15 15:08:27 +0100

    Fix code: mancava un punto e virgola

--- src/main/webapp/app/components/task-list/task-list.directive.js
+++ src/main/webapp/app/components/task-list/task-list.directive.js
@@ -1,39 +1,39 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('sprintApp')
-  .directive('taskList', taskListDirective);
+    angular.module('sprintApp')
+        .directive('taskList', taskListDirective);
 
-  taskListDirective.$inject = ['dataService', '$log', 'AlertService'];
+    taskListDirective.$inject = ['dataService', '$log', 'AlertService'];
 
-  function taskListDirective(dataService, $log, AlertService) {
+    function taskListDirective(dataService, $log, AlertService) {
 
-    return {
-      restrict: 'E',
-      scope: {
-        tasks: '='
-      },
-      templateUrl: 'app/components/task-list/task-list.html',
-      link: function (scope, element, attrs) {
+        return {
+            restrict: 'E',
+            scope: {
+                tasks: '='
+            },
+            templateUrl: 'app/components/task-list/task-list.html',
+            link: function (scope, element, attrs) {
 
-          scope.actionButtons = attrs.actionButtons;
+                scope.actionButtons = attrs.actionButtons;
 
-        scope.completed = attrs.completed;
+                scope.completed = attrs.completed;
 
-        scope.pooled = [];
+                scope.pooled = [];
 
-        scope.claimTask = function (taskId, take) {
-          var user;
-          dataService.tasks.claim(taskId, take).then(function (data) {
-            $log.debug(data);
-            scope.pooled[taskId] = user !== undefined;
-            scope.$parent.loadTasks();
-          }, function (err) {
-            $log.error(err);
-            AlertService.error("Richiesta non riuscita<br>"+ err.data.message);
-          });
+                scope.claimTask = function (taskId, take) {
+                    var user;
+                    dataService.tasks.claim(taskId, take).then(function (data) {
+                        $log.debug(data);
+                        scope.pooled[taskId] = user !== undefined;
+                        scope.$parent.loadTasks();
+                    }, function (err) {
+                        $log.error(err);
+                        AlertService.error("Richiesta non riuscita<br>" + err.data.message);
+                    });
+                };
+            }
         };
-      }
-    };
-  }
+    }
 })();
