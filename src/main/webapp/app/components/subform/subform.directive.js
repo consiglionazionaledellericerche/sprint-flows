a9306e7d42aa8d3508f1ab79dcd0dd5065e0c01c
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2017-12-19 11:24:13 +0100

    Aggiunta documentazione a subform. Migliore inizializzazione parametri

--- src/main/webapp/app/components/subform/subform.directive.js
+++ src/main/webapp/app/components/subform/subform.directive.js
@@ -1,6 +1,35 @@
 (function() {
     'use strict';
 
+    /**
+     * Author: Martin
+     *
+     *  Uso:
+     *  ----
+     *
+     *
+     *  Inserire un entita' form nel db (mettiamo col nome X)
+     *
+     *  richiamare la form in un altra form con
+     *
+     *  <subform
+     *    multiple=true
+     *    subform-name="X"
+     *    label="Questa e' una subform"
+     *    ng-model="vm.data.nomeDelModel"
+     *    json="'[{}]'" />
+     *
+     *  Il parametro json serve per inizializzare il valore della subform ed e' una stringa
+     *
+     *  Per inizializzare una subform vuota usare
+     *  json="'[{}]'" (ricordarsi le virgolette interne - e' una stringa)
+     *
+     *  Per inizializzare una subform con parametri inseriti precedentemente usare (per esempio)
+     *  json="vm.data.entity.variabili['nomeDelModel_json']"
+     *
+     *  Con questo meccanismo e' possibile inizializzare il model con qualunque valore arbitrario
+     */
+
     angular.module('sprintApp')
     .directive('subform', subform);
 
