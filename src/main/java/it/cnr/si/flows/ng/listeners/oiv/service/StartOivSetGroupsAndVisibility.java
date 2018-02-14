476e340456cf5f02e1a3338d70c33e3f0947a012
massimo.fraticelli <massimo.fraticelli@cnr.it>
2018-02-14 15:33:12 +0100

    gestione del date time esperienze

--- src/main/java/it/cnr/si/flows/ng/listeners/oiv/service/StartOivSetGroupsAndVisibility.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/oiv/service/StartOivSetGroupsAndVisibility.java
@@ -2,57 +2,96 @@ package it.cnr.si.flows.ng.listeners.oiv.service;
 
 
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.delegate.BpmnError;
 import org.activiti.engine.delegate.DelegateExecution;
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import it.cnr.si.flows.ng.TestServices;
 
 import javax.inject.Inject;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-
+import static it.cnr.si.flows.ng.TestServices.TITOLO_DELL_ISTANZA_DEL_FLUSSO;
+import static it.cnr.si.flows.ng.utils.Enum.VariableEnum.initiator;
+import static it.cnr.si.flows.ng.utils.Enum.VariableEnum.oggetto;
 import static it.cnr.si.flows.ng.utils.Utils.PROCESS_VISUALIZER;
+import static org.junit.Assert.assertEquals;
 
 @Service
 public class StartOivSetGroupsAndVisibility {
-    private static final Logger LOGGER = LoggerFactory.getLogger(StartOivSetGroupsAndVisibility.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(StartOivSetGroupsAndVisibility.class);
 
 
-    @Inject
-    private RuntimeService runtimeService;
+	@Inject
+	private RuntimeService runtimeService;
 
-    public void configuraVariabiliStart(DelegateExecution execution)  throws IOException, ParseException  {
+	public void configuraVariabiliStart(DelegateExecution execution)  throws IOException, ParseException  {
 
-        String initiator = (String) execution.getVariable("initiator");
-        LOGGER.info("L'utente {} sta avviando il flusso {} (con titolo {})", initiator, execution.getId(), execution.getVariable("title"));
+		String initiator = (String) execution.getVariable("initiator");
+		LOGGER.info("L'utente {} sta avviando il flusso {} (con titolo {})", initiator, execution.getId(), execution.getVariable("title"));
 
-        String struttura = "99999";
-        String gruppoIstruttori = "istruttore@"+ struttura;
-        String gruppoDirettore = "direttore@"+ struttura;
-        String gruppoCoordinatoreResponsabile = "coordinatoreresponsabile@"+ struttura;
+		String struttura = "99999";
+		String gruppoIstruttori = "istruttore@"+ struttura;
+		String gruppoDirettore = "direttore@"+ struttura;
+		String gruppoCoordinatoreResponsabile = "coordinatoreresponsabile@"+ struttura;
 
-        execution.setVariable("gruppoIstruttori", gruppoIstruttori);
-        execution.setVariable("gruppoDirettore", gruppoDirettore);
-        execution.setVariable("gruppoCoordinatoreResponsabile", gruppoCoordinatoreResponsabile);
-        execution.setVariable("domandaImprocedibileFlag", "0");
-        execution.setVariable("soccorsoIstruttoriaFlag", "0");
-        LOGGER.debug("Imposto i gruppi del flusso {}, {}, {}, {}", gruppoIstruttori, gruppoDirettore, gruppoCoordinatoreResponsabile);
+		execution.setVariable("gruppoIstruttori", gruppoIstruttori);
+		execution.setVariable("gruppoDirettore", gruppoDirettore);
+		execution.setVariable("gruppoCoordinatoreResponsabile", gruppoCoordinatoreResponsabile);
+		execution.setVariable("domandaImprocedibileFlag", "0");
+		execution.setVariable("soccorsoIstruttoriaFlag", "0");
+		LOGGER.debug("Imposto i gruppi del flusso {}, {}, {}, {}", gruppoIstruttori, gruppoDirettore, gruppoCoordinatoreResponsabile);
 
-        //conversione semplice di date
-//		if (execution.getVariable("valutazioneEsperienze_json") !=  null) {
-//			String valutazioneEsperienze_json = execution.getVariable("valutazioneEsperienze_json").toString();
-//			valutazioneEsperienze_json = valutazioneEsperienze_json.replaceAll("T23:00:00.000Z", "");
-//			execution.setVariable("valutazioneEsperienze_json", valutazioneEsperienze_json);
+		//conversione semplice di date
+		//if (execution.getVariable("valutazioneEsperienze_json") !=  null) {
+		//String valutazioneEsperienze_json = execution.getVariable("valutazioneEsperienze_json").toString();
+		//	valutazioneEsperienze_json = valutazioneEsperienze_json.replaceAll("T23:00:00.000Z", "");
+		//	execution.setVariable("valutazioneEsperienze_json", valutazioneEsperienze_json);
+		//}
+
+
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+//		String dataInizioStr = "00-00-0000";
+//		String dataFineStr = "00-00-0000";
+//		Date dataInizioShort = sdf.parse("00-00-0000");
+//		Date dataFineShort = sdf.parse("00-00-0000");
+//		String valutazioneEsperienzeString = (String) execution.getVariable("valutazioneEsperienze_json");
+//		JSONArray valutazioneEsperienze = new JSONArray();
+//		try {
+//			valutazioneEsperienze = new JSONArray(valutazioneEsperienzeString);
+//
+//			for ( int i = 0; i < valutazioneEsperienze.length(); i++) {
+//				JSONObject esperienza = new JSONObject();
+//				esperienza = valutazioneEsperienze.getJSONObject(i);
+//				dataInizioStr = esperienza.getString("dataInizio");
+//				dataInizioShort = sdf.parse(dataInizioStr);
+//				esperienza.put("dataInizioShort", i);
+//				dataFineStr = esperienza.getString("dataFine");
+//				dataFineShort = sdf.parse(dataFineStr);
+//				esperienza.put("dataFineShort", i);
+//			}
+//		} catch (JSONException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
 //		}
+//		execution.setVariable("valutazioneEsperienze_json", valutazioneEsperienze.toString());
 
-        LOGGER.info("------ DATA FINE PROCEDURA: " + execution.getVariable("dataScadenzaTerminiDomanda"));
-        runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoIstruttori, PROCESS_VISUALIZER);
-        runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoDirettore, PROCESS_VISUALIZER);
-        runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoCoordinatoreResponsabile, PROCESS_VISUALIZER);
-        runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoCoordinatoreResponsabile, PROCESS_VISUALIZER);
 
-    }
+
+		LOGGER.info("------ DATA FINE PROCEDURA: " + execution.getVariable("dataScadenzaTerminiDomanda"));
+		runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoIstruttori, PROCESS_VISUALIZER);
+		runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoDirettore, PROCESS_VISUALIZER);
+		runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoCoordinatoreResponsabile, PROCESS_VISUALIZER);
+		runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoCoordinatoreResponsabile, PROCESS_VISUALIZER);
+
+	}
 }
