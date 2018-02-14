37dcd246a41c590850fd79aacd2cb9daf37e7c41
massimo.fraticelli <massimo.fraticelli@cnr.it>
2018-02-14 18:34:02 +0100

    fine tuning printpdf

--- src/main/java/it/cnr/si/flows/ng/listeners/oiv/service/CreateOivPdf.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/oiv/service/CreateOivPdf.java
@@ -1,6 +1,8 @@
 package it.cnr.si.flows.ng.listeners.oiv.service;
 
 import it.cnr.si.flows.ng.dto.FlowsAttachment;
+import it.cnr.si.flows.ng.resource.FlowsPdfResource;
+
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.ExecutionListener;
@@ -28,38 +30,43 @@ import static it.cnr.si.flows.ng.utils.Enum.Azione.Caricamento;
 public class CreateOivPdf  {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CreateOivPdf.class);
 
+//	@Inject
+//	private OivPdfService oivPdfService;
 	@Inject
-	private OivPdfService oivPdfService;
+	private FlowsPdfResource flowsPdfResource;
 	
 	public void CreaPdfOiv(DelegateExecution execution, String tipologiaDoc) throws IOException, ParseException {
 		//(OivPdfService oivPdfService = new OivPdfService();
 		
 		
-		String processInstanceId =  execution.getProcessInstanceId();		
+		String processInstanceId =  execution.getProcessInstanceId();	
 		LOGGER.info("ProcessInstanceId: " + processInstanceId);
-		LOGGER.info("STAMPA la seguente tipologia di documento: " + processInstanceId);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        String fileName = null;
-        try {
-            fileName = oivPdfService.createPdf(processInstanceId, outputStream, tipologiaDoc);
-        } catch (IOException | ParseException e) {
-            LOGGER.error("Errore nella creazione del file pdf  per la Process Instance {}. \n" +
-                                 "Errore: {}", processInstanceId, e.getMessage());
-        }
-        //String variableFileName = fileName.replaceAll(" ", "_");
-        //variableFileName = variableFileName.replace(".pdf", "");
-        String variableFileName = tipologiaDoc;
-		LOGGER.info("avvio la generazione del pdf: " + fileName + " con variabile: " + variableFileName);
-        FlowsAttachment pdfToDB = new FlowsAttachment();
-        pdfToDB.setBytes(outputStream.toByteArray());
-        pdfToDB.setAzione(Caricamento);
-        pdfToDB.setTaskId(null);
-        pdfToDB.setTaskName(null);
-        pdfToDB.setTime(new Date());
-        pdfToDB.setName(variableFileName);
-        pdfToDB.setFilename(fileName);
-        pdfToDB.setMimetype(MediaType.PDF.toString());
-
-        execution.setVariable(variableFileName, pdfToDB);
+		LOGGER.info("STAMPA la seguente tipologia di documento: " + tipologiaDoc);
+		
+		flowsPdfResource.makePdf(processInstanceId, tipologiaDoc);
+		
+		
+//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//        String fileName = null;
+//        try {
+//            fileName = oivPdfService.createPdf(processInstanceId, outputStream, tipologiaDoc);
+//        } catch (IOException | ParseException e) {
+//            LOGGER.error("Errore nella creazione del file pdf  per la Process Instance {}. \n" +
+//                                 "Errore: {}", processInstanceId, e.getMessage());
+//        }
+//        //String variableFileName = fileName.replaceAll(" ", "_");
+//        //variableFileName = variableFileName.replace(".pdf", "");
+//        String variableFileName = tipologiaDoc;
+//		LOGGER.info("avvio la generazione del pdf: " + fileName + " con variabile: " + variableFileName);
+//        FlowsAttachment pdfToDB = new FlowsAttachment();
+//        pdfToDB.setBytes(outputStream.toByteArray());
+//        pdfToDB.setAzione(Caricamento);
+//        pdfToDB.setTaskId(null);
+//        pdfToDB.setTaskName(null);
+//        pdfToDB.setTime(new Date());
+//        pdfToDB.setName(variableFileName);
+//        pdfToDB.setFilename(fileName);
+//        pdfToDB.setMimetype(MediaType.PDF.toString());
+//        execution.setVariable(variableFileName, pdfToDB);
 
 	}}
