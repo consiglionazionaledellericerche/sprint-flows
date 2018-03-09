d41f3311728783540bef435780193efed8c7115f
massimo.fraticelli <massimo.fraticelli@cnr.it>
2018-03-09 17:17:34 +0100

    gestione telefono utente e stampa

--- src/main/java/it/cnr/si/flows/ng/listeners/oiv/service/DeterminaAttore.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/oiv/service/DeterminaAttore.java
@@ -7,6 +7,8 @@ import org.activiti.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import it.cnr.si.service.FlowsUserService;
 import it.cnr.si.service.UserService;
 
 
@@ -17,17 +19,29 @@ public class DeterminaAttore {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DeterminaAttore.class);
 
     @Inject
-    private UserService userService;
+    private FlowsUserService flowsUserService;
 
 	public void determinaIstruttore(DelegateExecution execution) throws IOException, ParseException {
 		
-		String currentName = userService.getUserWithAuthorities().getFirstName() + " " + userService.getUserWithAuthorities().getLastName();
-		String currentUsername = userService.getUserWithAuthorities().getLogin();
+		String currentName = flowsUserService.getUserWithAuthorities().getFirstName() + " " + flowsUserService.getUserWithAuthorities().getLastName();
+		String currentUsername = flowsUserService.getUserWithAuthorities().getLogin();
+		String userPhone = "1";
+		String sessoIstruttore = "m";
+		if(flowsUserService.getUserWithAuthorities().getPhone() != null){
+		    userPhone = flowsUserService.getUserWithAuthorities().getPhone();
+		}
+//		if(flowsUserService.getUserWithAuthorities().getGender() != null){
+//			sessoIstruttore = flowsUserService.getUserWithAuthorities().getGender();
+//		}
 
 		execution.setVariable("nomeIstruttore", currentName);
 		execution.setVariable("istruttoreIncaricato", currentUsername);
-		LOGGER.debug("--- nomeIstruttore: {} - istruttoreIncaricato: {}", currentName, currentUsername);
+		execution.setVariable("telefonoIstruttore", userPhone);
+		execution.setVariable("sessoIstruttore", sessoIstruttore);
+		
+		LOGGER.debug("--- nomeIstruttore: {} - istruttoreIncaricato: {} - telefonoIstruttore: {}- sessoIstruttore: {}", currentName, currentUsername, userPhone, sessoIstruttore);
 
 	}
 
 }
+
