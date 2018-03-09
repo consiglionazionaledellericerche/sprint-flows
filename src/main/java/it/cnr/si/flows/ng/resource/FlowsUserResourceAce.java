134b05e330798604fb94c8a4f4e2fb03b5b51608
paoloenricocirone <cironepa@gmail.com>
2018-03-09 15:16:54 +0100

    Customizzazione della gestione degli utenti dell'applicazione: Fix import e package FlowsUserResourceAce.java

--- src/main/java/it/cnr/si/flows/ng/resource/FlowsUserResourceAce.java
+++ src/main/java/it/cnr/si/flows/ng/resource/FlowsUserResourceAce.java
@@ -1,35 +1,35 @@
 package it.cnr.si.flows.ng.resource;
 
 import com.codahale.metrics.annotation.Timed;
-import it.cnr.si.config.Constants;
-import it.cnr.si.domain.Authority;
-import it.cnr.si.domain.FlowsUser;
-import it.cnr.si.flows.ng.dto.FlowsUserDto;
-import it.cnr.si.flows.ng.service.FlowsMailService;
-import it.cnr.si.repository.FlowsUserRepository;
+import it.cnr.si.flows.ng.repository.FlowsHistoricProcessInstanceQuery;
+import it.cnr.si.flows.ng.service.AceBridgeService;
 import it.cnr.si.security.AuthoritiesConstants;
-import it.cnr.si.service.FlowsUserService;
-import it.cnr.si.web.rest.UserResource;
-import it.cnr.si.web.rest.util.HeaderUtil;
-import it.cnr.si.web.rest.util.PaginationUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.activiti.engine.ManagementService;
+import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.impl.interceptor.Command;
+import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.rest.service.api.RestResponseFactory;
+import org.activiti.rest.service.api.history.HistoricProcessInstanceResponse;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ldap.core.AttributesMapper;
+import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import java.net.URI;
-import java.net.URISyntaxException;
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -38,162 +38,107 @@ import java.util.stream.Collectors;
  * (e non quello di sprint-core) perchè abbiamo dei campi customizzati (phone).
  */
 @RestController
-@RequestMapping("/api/flows")
+@RequestMapping("api/users")
 public class FlowsUserResourceAce {
 
-    private final Logger log = LoggerFactory.getLogger(UserResource.class);
+    @Autowired
+    private LdapTemplate ldapTemplate;
 
     @Inject
-    private FlowsUserRepository flowsUserRepository;
+    private AceBridgeService aceService;
+
     @Inject
-    private FlowsMailService flowsMailService;
+    private ManagementService managementService;
+
     @Inject
-    private FlowsUserService flowsUserService;
+    private RestResponseFactory restResponseFactory;
 
-
-    /**
-     * POST  /users  : Crea un nuovo FlowsUser.
-     * <p>
-     * Crea un nuovo FlowsUser se il login e la email non sono usati da altri FlowsUser
-     * </p>
-     *
-     * @param flowsUser il FlowsUser da creare
-     * @param request   HTTP request
-     * @return la ResponseEntity con status 201 (Creato) e con il nuovo FlowsUser nel body, o con status 400 (Bad Request) se il login o la email sono già in uso
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @RequestMapping(value = "/users",
-            method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
+    @RequestMapping(value = "/ace/user/{username:.+}", method = RequestMethod.GET)
     @Secured(AuthoritiesConstants.ADMIN)
-    public ResponseEntity<FlowsUser> createUser(
-            @RequestBody FlowsUserDto flowsUser,
-            HttpServletRequest request) throws URISyntaxException {
-        log.debug("REST request to save User : {}", flowsUser);
-
-        //Lowercase the user login before comparing with database
-        if (flowsUserRepository.findOneByLogin(flowsUser.getLogin().toLowerCase()).isPresent()) {
-            return ResponseEntity.badRequest()
-                    .headers(HeaderUtil.createFailureAlert("userManagement", "userexists", "Login already in use"))
-                    .body(null);
-        } else if (flowsUserRepository.findOneByEmail(flowsUser.getEmail()).isPresent()) {
-            return ResponseEntity.badRequest()
-                    .headers(HeaderUtil.createFailureAlert("userManagement", "emailexists", "Email already in use"))
-                    .body(null);
-        } else {
-            FlowsUserDto flowsUserDto = new FlowsUserDto(flowsUser.getId(), flowsUser.getLogin(), flowsUser.getPassword(), flowsUser.getFirstName(),
-                                                         flowsUser.getLastName(), flowsUser.getEmail(), flowsUser.isActivated(), flowsUser.getLangKey(),
-                                                         flowsUser.getAuthorities(), flowsUser.getCreatedDate(), flowsUser.getLastModifiedBy(), flowsUser.getLastModifiedDate(), flowsUser.getPhone());
-
-            FlowsUser newFlowsUser = flowsUserService.createUser(flowsUserDto);
-
-            return ResponseEntity.created(new URI("/api/users/" + newFlowsUser.getLogin()))
-                    .headers(HeaderUtil.createAlert("userManagement.created", newFlowsUser.getLogin()))
-                    .body(newFlowsUser);
-        }
+    public List<String> getAce(@PathVariable String username) throws SQLException {
+        return aceService.getAceGroupsForUser(username);
     }
 
-    /**
-     * PUT  /users : Aggiorna un FlowsUser che esiste.
-     *
-     * @param flowsUserDto il FlowsUser da aggiornare
-     * @return la ResponseEntity con status 200 (OK) ed il FlowsUser aggiornato nel body,
-     * o con status 400 (Bad Request) se il login o la email è già in uso,
-     * o con status 500 (Internal Server Error) se lo FlowsUser non può essere aggiornato
-     */
-    @RequestMapping(value = "/users",
-            method = RequestMethod.PUT,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
+    @RequestMapping(value = "/ace/group/{groupname:.+}", method = RequestMethod.GET)
     @Secured(AuthoritiesConstants.ADMIN)
-    public ResponseEntity<FlowsUserDto> updateUser(@RequestBody FlowsUserDto flowsUserDto) {
-        log.debug("REST request to update User : {}", flowsUserDto);
-        Optional<FlowsUser> existingUser = flowsUserRepository.findOneByEmail(flowsUserDto.getEmail());
-        if (existingUser.isPresent() && (!existingUser.get().getId().equals(flowsUserDto.getId()))) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("userManagement", "emailexists", "E-mail already in use")).body(null);
-        }
-        existingUser = flowsUserRepository.findOneByLogin(flowsUserDto.getLogin().toLowerCase());
-        if (existingUser.isPresent() && (!existingUser.get().getId().equals(flowsUserDto.getId()))) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("userManagement", "userexists", "Login already in use")).body(null);
-        }
-        flowsUserService.updateUser(flowsUserDto.getId(), flowsUserDto.getLogin(), flowsUserDto.getFirstName(),
-                                    flowsUserDto.getLastName(), flowsUserDto.getEmail(), flowsUserDto.isActivated(),
-                                    flowsUserDto.getLangKey(), flowsUserDto.getAuthorities(), flowsUserDto.getPhone());
-
-        FlowsUserDto newFlowsUserDto = new FlowsUserDto(flowsUserDto.getId(), flowsUserDto.getLogin(), flowsUserDto.getPassword(),
-                                                        flowsUserDto.getFirstName(), flowsUserDto.getLastName(), flowsUserDto.getEmail(),
-                                                        flowsUserDto.isActivated(), flowsUserDto.getLangKey(), flowsUserDto.getAuthorities(),
-                                                        flowsUserDto.getCreatedDate(), flowsUserDto.getLastModifiedBy(),
-                                                        flowsUserDto.getLastModifiedDate(), flowsUserDto.getPhone());
-        return ResponseEntity.ok()
-                .headers(HeaderUtil.createAlert("userManagement.updated", flowsUserDto.getLogin()))
-                .body(newFlowsUserDto);
+    public List<String> getAceGroup(@PathVariable String groupname) throws SQLException {
+        return aceService.getUsersinAceGroup(groupname);
     }
 
-    /**
-     * GET  /users : get all FlowsUser.
-     *
-     * @param pageable the pagination information
-     * @return the ResponseEntity with status 200 (OK) and with body all FlowsUser
-     * @throws URISyntaxException if the pagination headers couldn't be generated
-     */
-    @RequestMapping(value = "/users",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<List<FlowsUserDto>> getAllUsers(Pageable pageable)
-            throws URISyntaxException {
-        Page<FlowsUser> page = flowsUserRepository.findAllWithAuthorities(pageable);
-        List<FlowsUserDto> flowsUserDtos = page.getContent().stream().map(
-                this::getFlowsUserDto)
-                .collect(Collectors.toList());
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/users");
-        return new ResponseEntity<>(flowsUserDtos, headers, HttpStatus.OK);
-    }
-
-    /**
-     * GET  /users/:login : get the "login" FlowsUser.
-     *
-     * @param login the login of the FlowsUser to find
-     * @return the ResponseEntity with status 200 (OK) and with body the "login" FlowsUser, or with status 404 (Not Found)
-     */
-    @RequestMapping(value = "/users/{login:" + Constants.LOGIN_REGEX + "}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<FlowsUserDto> getUser(@PathVariable String login) {
-        log.debug("REST request to get User : {}", login);
-        return flowsUserService.getUserWithAuthoritiesByLogin(login).map(
-                this::getFlowsUserDto)
-                .map(flowsUserDto -> new ResponseEntity<>(flowsUserDto, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
-    private FlowsUserDto getFlowsUserDto(FlowsUser flowsUserDto) {
-        Set<String> authorities = flowsUserDto.getAuthorities().stream()
-                .map(Authority::getName)
-                .collect(Collectors.toSet());
-        return new FlowsUserDto(flowsUserDto.getId(), flowsUserDto.getLogin(), flowsUserDto.getPassword(), flowsUserDto.getFirstName(),
-                                flowsUserDto.getLastName(), flowsUserDto.getEmail(), flowsUserDto.getActivated(), flowsUserDto.getLangKey(),
-                                authorities, flowsUserDto.getCreatedDate(), flowsUserDto.getLastModifiedBy(),
-                                flowsUserDto.getLastModifiedDate(), flowsUserDto.getPhone());
-    }
-
-    /**
-     * DELETE /users/:login : delete the "login" FlowsUser.
-     *
-     * @param login the login of the FlowsUser to delete
-     * @return the ResponseEntity with status 200 (OK)
-     */
-    @RequestMapping(value = "/users/{login:" + Constants.LOGIN_REGEX + "}",
-            method = RequestMethod.DELETE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
+    @RequestMapping(value = "/ace/groupdetail/{id}", method = RequestMethod.GET)
     @Secured(AuthoritiesConstants.ADMIN)
-    public ResponseEntity<Void> deleteUser(@PathVariable String login) {
-        log.debug("REST request to delete User: {}", login);
-        flowsUserService.deleteUser(login);
-        return ResponseEntity.ok().headers(HeaderUtil.createAlert("userManagement.deleted", login)).build();
+    public String getAceGroupDetail(@PathVariable Integer id) throws SQLException {
+        return aceService.getNomeStruturaById(id);
+    }
+
+
+    @RequestMapping(value = "/{username:.+}/search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Secured(AuthoritiesConstants.USER)
+    @Timed
+    public ResponseEntity<Map<String, Object>> getMyTasks(@PathVariable String username) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        List<SearchResult> search = ldapTemplate.search("", "(uid=*" + username + "*)", new AttributesMapper<SearchResult>() {
+            public SearchResult mapFromAttributes(Attributes attrs) throws NamingException {
+                return new SearchResult(attrs.get("uid").get().toString(),
+                                        attrs.get("cnrnome").get() + " " + attrs.get("cnrcognome").get() + " " +
+                                                "(" + attrs.get("uid").get().toString() + ")");
+            }
+        });
+
+        response.put("more", search.size() > 10);
+        response.put("results", search.stream().limit(10).collect(Collectors.toList()));
+
+        return ResponseEntity.ok(response);
+    }
+
+    @RequestMapping(value = "/struttura/{struttura:.+}/search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Secured(AuthoritiesConstants.USER)
+    @Timed
+    public ResponseEntity<Map<String, Object>> getUoLike(@PathVariable String struttura) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        List<Pair<Integer, String>> results = aceService.getUoLike(struttura);
+        List<SearchResult> collect = results.stream().map(p -> new SearchResult(p.getLeft().toString(), p.getRight())).collect(Collectors.toList());
+
+        response.put("more", collect.size() > 10);
+        response.put("results", collect.stream().limit(10).collect(Collectors.toList()));
+
+        return ResponseEntity.ok(response);
+    }
+
+    //    todo: non vieme mai usato => cancellare?
+    @RequestMapping(value = "/customquery", method = RequestMethod.GET)
+    @Secured(AuthoritiesConstants.ADMIN)
+    public List<HistoricProcessInstanceResponse> customQuery() throws SQLException {
+
+        FlowsHistoricProcessInstanceQuery query = new FlowsHistoricProcessInstanceQuery(managementService);
+        List<String> groups = new ArrayList<>();
+        groups.add("sfd@2216");
+        query.setVisibleToGroups(groups);
+
+        List<HistoricProcessInstance> processes = managementService.executeCommand(new Command<List<HistoricProcessInstance>>() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public List<HistoricProcessInstance> execute(CommandContext commandContext) {
+                return (List<HistoricProcessInstance>) commandContext.getDbSqlSession().selectList("selectFlowsHistoricProcessInstancesWithVariablesByQueryCriteria", query);
+            }
+        });
+
+        return restResponseFactory.createHistoricProcessInstanceResponseList(processes);
+    }
+
+
+    public class SearchResult {
+        public String value;
+        public String label;
+
+        public SearchResult(String v, String l) {
+            this.value = v;
+            this.label = l;
+        }
     }
 }
