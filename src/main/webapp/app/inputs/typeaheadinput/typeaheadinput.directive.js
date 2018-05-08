2bdd6f7e89f2a18dda402b3d1b115d3ae6eb4ae2
paoloenricocirone <cironepa@gmail.com>
2018-05-08 16:17:06 +0200

    typeaheadinput: aggiunta nuova tipologia per l'autocompletamento sui flowsGroups (gruppi ne Db)

--- src/main/webapp/app/inputs/typeaheadinput/typeaheadinput.directive.js
+++ src/main/webapp/app/inputs/typeaheadinput/typeaheadinput.directive.js
@@ -1,63 +1,69 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('sprintApp')
-    .directive('typeaheadinput', typeaheadinput);
+    angular.module('sprintApp')
+        .directive('typeaheadinput', typeaheadinput);
 
-  typeaheadinput.$inject = ['dataService', '$log'];
+    typeaheadinput.$inject = ['dataService', '$log'];
 
-  /**
-   * Questa direttiva e' un semplicissimo typeahead con dei parametri preimpostati
-   * L'API e' semplicissima: ng-model e ng-required
-   */
-  function typeaheadinput(dataService, $log) {
+    /**
+     * Questa direttiva e' un semplicissimo typeahead con dei parametri preimpostati
+     * L'API e' semplicissima: ng-model e ng-required
+     */
+    function typeaheadinput(dataService, $log) {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/inputs/typeaheadinput/typeaheadinput.html',
+            scope: {
+                ngModel: '=',
+                labelModel: '=',
+                type: '@',
+                ngRequired: '@',
+            },
+            link: function($scope, element, attrs) {
+                $scope.localModel = undefined;
 
-    return {
-      restrict: 'E',
-      templateUrl: 'app/inputs/typeaheadinput/typeaheadinput.html',
-      scope: {
-        ngModel: '=',
-        labelModel: '=',
-        type: '@',
-        ngRequired: '@'
-      },
-      link: function($scope, element, attrs) {
-        $scope.localModel = undefined;
-
-        $scope.$watch('localModel', function(newVal) {
-          if (newVal) {
-            $scope.ngModel = newVal.value;
-            $scope.labelModel = newVal.label;
-          }
-        });
-
-        $scope.loadRecords = function(filter) {
-          switch ($scope.type) {
-            case 'users':
-            return dataService.search.users(filter)
-              .then(function(response) {
-                $scope.hasMore = response.data.more;
-                return response.data.results;
-              });
-            break;
-            case 'flowsUsers':
-              return dataService.search.flowsUsers(filter)
-                .then(function(response) {
-                  $scope.hasMore = response.data.more;
-                  return response.data.results;
+                $scope.$watch('localModel', function(newVal) {
+                    if (newVal) {
+                        $scope.ngModel = newVal.value;
+                        $scope.labelModel = newVal.label;
+                    }
                 });
-              break;
-            case 'uo':
-              return dataService.search.uo(filter)
-                .then(function(response) {
-                  $scope.hasMore = response.data.more;
-                  return response.data.results;
-                });
-            default:
-              $log.error('Type non riconosciuto ' + $scope.type);
-          }
+
+                $scope.loadRecords = function(filter) {
+                    switch ($scope.type) {
+                        case 'users':
+                            return dataService.search.users(filter)
+                                .then(function(response) {
+                                    $scope.hasMore = response.data.more;
+                                    return response.data.results;
+                                });
+                            break;
+                        case 'uo':
+                            return dataService.search.uo(filter)
+                                .then(function(response) {
+                                    $scope.hasMore = response.data.more;
+                                    return response.data.results;
+                                });
+                        case 'flowsUsers':
+                            return dataService.search.flowsUsers(filter)
+                                .then(function(response) {
+                                    $scope.hasMore = response.data.more;
+                                    return response.data.results;
+                                });
+                            break;
+                        case 'flowsGroups':
+                            return dataService.search.flowsGroups(filter)
+                                .then(function(response) {
+                                    $scope.hasMore = response.data.more;
+                                    return response.data.results;
+                                });
+                            break;
+                        default:
+                            $log.error('Type non riconosciuto ' + $scope.type);
+                    }
+                };
+            },
         };
-      },
-    };
-  }
+    }
 })();
