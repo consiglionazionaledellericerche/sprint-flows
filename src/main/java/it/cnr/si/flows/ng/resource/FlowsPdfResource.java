d9e635540c402b4311707388565da853a7e7f53f
paoloenricocirone <cironepa@gmail.com>
2018-05-15 16:02:29 +0200

    Issue #114: pagina per le esportazioni delle statistiche (pdf e csv):   -  FlowsPdfStatisticService.java viene spostato in FlowsPdfService.java   -  Codice di FlowsCsvResource.makeStatisticCsv e FlowsPdfResource.makeStatisticPdf vengono uniformati nella response   -  Pulizia codice

--- src/main/java/it/cnr/si/flows/ng/resource/FlowsPdfResource.java
+++ src/main/java/it/cnr/si/flows/ng/resource/FlowsPdfResource.java
@@ -2,12 +2,11 @@ package it.cnr.si.flows.ng.resource;
 
 
 import com.codahale.metrics.annotation.Timed;
-
 import it.cnr.si.flows.ng.listeners.oiv.service.StatisticOivService;
 import it.cnr.si.flows.ng.service.FlowsAttachmentService;
 import it.cnr.si.flows.ng.service.FlowsPdfService;
-import it.cnr.si.flows.ng.service.FlowsPdfStatisticService;
 import it.cnr.si.flows.ng.utils.Enum;
+import it.cnr.si.flows.ng.utils.Utils;
 import it.cnr.si.security.AuthoritiesConstants;
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.RuntimeService;
@@ -34,194 +33,170 @@ import java.io.ByteArrayOutputStream;
 import java.text.ParseException;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Calendar;
-import java.util.Date;
-import it.cnr.si.flows.ng.utils.Utils;
 
 
+/**
+ * The type Flows pdf resource.
+ */
 @Controller
 @RequestMapping("api")
 public class FlowsPdfResource {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FlowsPdfResource.class);
-    private static final String VALUTAZIONE_ESPERIENZE_JSON = "valutazioneEsperienze_json";
-    public static final String BYTES = "bytes";
-    @Inject
-    private FlowsPdfService pdfService;
-    @Inject
-    private FlowsAttachmentService flowsAttachmentService;
-    @Inject
-    private HistoryService historyService;
-    @Inject
-    private RuntimeService runtimeService;
-    @Inject
-    private StatisticOivService statisticOivService;
-    @Inject
-    private FlowsPdfStatisticService flowsPdfStatisticService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(FlowsPdfResource.class);
+	private static final String VALUTAZIONE_ESPERIENZE_JSON = "valutazioneEsperienze_json";
+
+	@Inject
+	private FlowsPdfService pdfService;
+	@Inject
+	private FlowsAttachmentService flowsAttachmentService;
+	@Inject
+	private HistoryService historyService;
+	@Inject
+	private RuntimeService runtimeService;
+	@Inject
+	private StatisticOivService statisticOivService;
 	@Inject
 	private Utils utils;
-    /**
-     * Crea e restituisce il summary pdf del flusso.
-     *
-     * @param processInstanceId : processInstanceId del workflow di cui si vuole generare il summary
-     * @return the response entity
-     * @throws Exception the exception
-     * @return: restituisce il pdf generato
-     */
-    @RequestMapping(value = "/summaryPdf", headers = "Accept=application/pdf", method = RequestMethod.GET, produces = "application/pdf")
-    @ResponseBody
-    @Timed
-    @Secured(AuthoritiesConstants.USER)
-    public ResponseEntity<byte[]> makeSummaryPdf(
-            @RequestParam("processInstanceId") String processInstanceId,
-            HttpServletRequest req) {
-
-        try {
-            final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            String fileName = pdfService.makeSummaryPdf(processInstanceId, outputStream);
-
-            HttpHeaders headers = new HttpHeaders();
-            ResponseEntity<byte[]> resp;
-            headers.set("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-            headers.setContentType(MediaType.parseMediaType("application/pdf"));
-            headers.setContentLength(outputStream.toByteArray().length);
-            resp = new ResponseEntity<>(outputStream.toByteArray(), headers, HttpStatus.OK);
-
-            return resp;
-        } catch (Exception e) {
-            LOGGER.error("Errore nella creazione del Summary.pdf per il flusso {}: ", processInstanceId, e);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 
 
-    /**
-     * Crea e restituisce il un pdf del tipo specificato.
-     *
-     * @param processInstanceId processInstanceId del flusso
-     * @param tipologiaDoc      la "tipologia" di pdf da create (ad es.: "rigetto")
 
-     * @return the response entity
-     * @throws Exception the exception
-     * @return: restituisce il pdf generato
-     */
-    @RequestMapping(value = "/makePdf", headers = "Accept=application/pdf", method = RequestMethod.GET, produces = "application/pdf")
-    @ResponseBody
-    @Timed
-    @Secured(AuthoritiesConstants.USER)
-    public ResponseEntity<byte[]> makePdf(
-            @RequestParam("processInstanceId") String processInstanceId,
-            @RequestParam("tipologiaDoc") String tipologiaDoc) {
-    	
-    	
-   	
+	/**
+	 * Crea e restituisce il summary pdf del flusso.
+	 *
+	 * @param processInstanceId : processInstanceId del workflow di cui si vuole generare il summary
+	 * @param req               the req
+	 * @return ResponseEntity restituisce il pdf generato
+	 */
+	@RequestMapping(value = "/summaryPdf", headers = "Accept=application/pdf", method = RequestMethod.GET, produces = "application/pdf")
+	@ResponseBody
+	@Timed
+	@Secured(AuthoritiesConstants.USER)
+	public ResponseEntity<byte[]> makeSummaryPdf(
+			@RequestParam("processInstanceId") String processInstanceId,
+			HttpServletRequest req) {
 
-        //Sotituisco la lista di variabili da quelle storiche (historicProcessInstance.getProcessVariables() )a quelle attuali (variableInstanceJson)
-        //JSONObject processvariables = mappingVariables(historicProcessInstance.getProcessVariables());
-		boolean porcessoterminato = false;
+		try {
+			final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			String fileName = pdfService.makeSummaryPdf(processInstanceId, outputStream);
+
+			HttpHeaders headers = new HttpHeaders();
+			ResponseEntity<byte[]> resp;
+			headers.set("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+			headers.setContentType(MediaType.parseMediaType("application/pdf"));
+			headers.setContentLength(outputStream.toByteArray().length);
+			resp = new ResponseEntity<>(outputStream.toByteArray(), headers, HttpStatus.OK);
+
+			return resp;
+		} catch (Exception e) {
+			LOGGER.error("Errore nella creazione del Summary.pdf per il flusso {}: ", processInstanceId, e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+
+
+	/**
+	 * Crea e restituisce il un pdf del tipo specificato.
+	 *
+	 * @param processInstanceId processInstanceId del flusso
+	 * @param tipologiaDoc      la "tipologia" di pdf da create (ad es.: "rigetto")
+	 * @return ResponseEntity restituisce il pdf generato
+	 */
+	@RequestMapping(value = "/makePdf", headers = "Accept=application/pdf", method = RequestMethod.GET, produces = "application/pdf")
+	@ResponseBody
+	@Timed
+	@Secured(AuthoritiesConstants.USER)
+	public ResponseEntity<byte[]> makePdf(
+			@RequestParam("processInstanceId") String processInstanceId,
+			@RequestParam("tipologiaDoc") String tipologiaDoc) {
+
+		//Sotituisco la lista di variabili da quelle storiche (historicProcessInstance.getProcessVariables() )a quelle attuali (variableInstanceJson)
 		JSONObject variableInstanceJson = new JSONObject();
-		
-        HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery()
-                .includeProcessVariables()
-                .processInstanceId(processInstanceId)
-                .singleResult();
-        
-        if(historicProcessInstance.getEndTime() != null){
-    		porcessoterminato = true;
 
-    	// Verifico se il workflow è terminato
-        }
-    	if(porcessoterminato){
-            //carico le processVariablwes e rimappo in formato json il campo stringa "valutazioneEsperienze_json"
-          variableInstanceJson = new JSONObject(historicProcessInstance.getProcessVariables());
-    	} else {
-            Map<String, VariableInstance> tutteVariabiliMap = runtimeService.getVariableInstances(processInstanceId);
-    		for (Entry<String, VariableInstance> entry : tutteVariabiliMap.entrySet()) {
-    		    String key = entry.getKey();
-    		    VariableInstance value = entry.getValue();
-    		    Object variableValuealue = value.getValue();
-    		    variableInstanceJson.put(key, variableValuealue);
-    		}
-    		LOGGER.info("variableInstanceJson: " + variableInstanceJson);
-    		
-    	}
+		HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery()
+				.includeProcessVariables()
+				.processInstanceId(processInstanceId)
+				.singleResult();
 
-		
-        //Sotituisco la lista di variabili da quelle storiche (historicProcessInstance.getProcessVariables() )a quelle attuali (variableInstanceJson)
-        JSONObject processvariables = mappingVariables(variableInstanceJson);
-     
-        //creo il pdf corrispondente
-        String utenteRichiedente = processvariables.getString("nomeRichiedente");
-        String fileName = tipologiaDoc + "-" + utenteRichiedente + ".pdf";
-        byte[] pdfByteArray = pdfService.makePdf(Enum.PdfType.valueOf(tipologiaDoc), processvariables, fileName, utenteRichiedente, processInstanceId);
-        //popolo gli headers della response
-        HttpHeaders headers = new HttpHeaders();
-        ResponseEntity<byte[]> resp;
-        headers.set("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-        headers.setContentType(MediaType.parseMediaType("application/pdf"));
-        headers.setContentLength(pdfByteArray.length);
-        resp = new ResponseEntity<>(pdfByteArray, headers, HttpStatus.OK);
+		// Verifico se il workflow sia terminato
+		if(historicProcessInstance.getEndTime() != null){
+			//carico le processVariables e rimappo in formato json il campo stringa "valutazioneEsperienze_json"
+			variableInstanceJson = new JSONObject(historicProcessInstance.getProcessVariables());
+		} else {
+			Map<String, VariableInstance> tutteVariabiliMap = runtimeService.getVariableInstances(processInstanceId);
+			for (Entry<String, VariableInstance> entry : tutteVariabiliMap.entrySet()) {
+				String key = entry.getKey();
+				VariableInstance value = entry.getValue();
+				Object variableValuealue = value.getValue();
+				variableInstanceJson.put(key, variableValuealue);
+			}
+			LOGGER.info("variableInstanceJson: {}", variableInstanceJson);
+		}
 
-        return resp;
-    }
-    
-    
-    @RequestMapping(value = "/makeStatisticPdf", headers = "Accept=application/pdf", method = RequestMethod.GET, produces = "application/pdf")
-    @ResponseBody
-    @Timed
-    @Secured(AuthoritiesConstants.USER)
-    public ResponseEntity<byte[]> makeStatisticPdf(
-            @RequestParam("processDefinitionKey") String processDefinitionKey,
-            @RequestParam("startDateGreat") String startDateGreat,
-    		@RequestParam("startDateLess") String startDateLess) throws ParseException {
-    	
-       	
-        //Sotituisco la lista di variabili da quelle storiche (historicProcessInstance.getProcessVariables() )a quelle attuali (variableInstanceJson)
-        //JSONObject processvariables = statisticOivService.getOivStatistics(processDefinitionKey, startDateGreat, startDateLess);
-        JSONObject processvariables = flowsPdfStatisticService.getPdfStatistics(processDefinitionKey, startDateGreat, startDateLess);
-     
-        //creo il pdf corrispondente
-        //String fileName = processDefinitionKey + "-Statistics";
-        String fileName = "statisticheGeneraliFlows";
-        byte[] pdfByteArray = pdfService.makeStatisticPdf(processvariables, fileName, processDefinitionKey);
-        //popolo gli headers della response
-        HttpHeaders headers = new HttpHeaders();
-        ResponseEntity<byte[]> resp;
-        headers.set("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-        headers.setContentType(MediaType.parseMediaType("application/pdf"));
-        headers.setContentLength(pdfByteArray.length);
-        resp = new ResponseEntity<>(pdfByteArray, headers, HttpStatus.OK);
+		//Sotituisco la lista di variabili da quelle storiche (historicProcessInstance.getProcessVariables() )a quelle attuali (variableInstanceJson)
+		JSONObject processVariables = mappingVariables(variableInstanceJson);
 
-        return resp;
-    }
+		//creo il pdf corrispondente
+		String utenteRichiedente = processVariables.getString("nomeRichiedente");
+		String fileName = tipologiaDoc + "-" + utenteRichiedente + ".pdf";
+		byte[] pdfByteArray = pdfService.makePdf(Enum.PdfType.valueOf(tipologiaDoc), processVariables, fileName, utenteRichiedente, processInstanceId);
 
-    //Sotituisco il mapping direttamente con il json delle variabili sttuali 
-    //private JSONObject mappingVariables(Map<String, Object> processVariables) {
-    private JSONObject mappingVariables(JSONObject variables) {
+		//popolo gli headers della response
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+		headers.setContentType(MediaType.parseMediaType("application/pdf"));
+		headers.setContentLength(pdfByteArray.length);
 
-        //refactoring della stringona contenete le esperienze in un jsonArray
-        if (variables.has(VALUTAZIONE_ESPERIENZE_JSON)) {
-            JSONArray esperienze = new JSONArray(variables.getString(VALUTAZIONE_ESPERIENZE_JSON));
-            variables.put(VALUTAZIONE_ESPERIENZE_JSON, esperienze);
-        }
+		return new ResponseEntity<>(pdfByteArray, headers, HttpStatus.OK);
+	}
 
-        //tolgo, nel json, i campi "byte" dei documenti
-//        if (variables.has("domanda"))
-//            variables.getJSONObject("domanda").remove(BYTES);
-//
-//        if (variables.has("cv"))
-//            variables.getJSONObject("cv").remove(BYTES);
-//
-//        if (variables.has("cartaIdentita"))
-//            variables.getJSONObject("cartaIdentita").remove(BYTES);
-//        //i bytes degli altri allegati continuano ad apparire nel json ma non posso verificarli tutti
-//        if (variables.has("allegati[0]"))
-//            variables.getJSONObject("allegati[0]").remove(BYTES);
 
-        return variables;
-    }
-    
-	private String formatDate(Date date) {
-		return date != null ? utils.formattaDataOra(date) : "";
+
+	/**
+	 * Make statistic pdf response entity.
+	 *
+	 * @param processDefinitionKey the process definition key
+	 * @param startDateGreat       the start date great
+	 * @param startDateLess        the start date less
+	 * @return the response entity
+	 * @throws ParseException the parse exception
+	 */
+//    todo:  TEST
+	@RequestMapping(value = "/makeStatisticPdf", headers = "Accept=application/pdf", method = RequestMethod.GET, produces = "application/pdf")
+	@ResponseBody
+	@Timed
+	@Secured(AuthoritiesConstants.USER)
+	public ResponseEntity<byte[]> makeStatisticPdf(
+			@RequestParam("processDefinitionKey") String processDefinitionKey,
+			@RequestParam("startDateGreat") String startDateGreat,
+			@RequestParam("startDateLess") String startDateLess) {
+
+		//Sotituisco la lista di variabili da quelle storiche (historicProcessInstance.getProcessVariables() )a quelle attuali (variableInstanceJson)
+		JSONObject processvariables = pdfService.getPdfStatistics(processDefinitionKey, startDateGreat, startDateLess);
+
+		//creo il pdf corrispondente
+		byte[] pdfByteArray = pdfService.makeStatisticPdf(processvariables, "statisticheGeneraliFlows");
+
+		//popolo gli headers della response
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Disposition", "attachment; filename=\"statistiche_" + processDefinitionKey + ".pdf\"");
+		headers.setContentType(MediaType.APPLICATION_PDF);
+		headers.setContentLength(pdfByteArray.length);
+
+		return new ResponseEntity<>(pdfByteArray, headers, HttpStatus.OK);
+	}
+
+
+
+	//Sotituisco il mapping direttamente con il json delle variabili sttuali
+	private JSONObject mappingVariables(JSONObject variables) {
+
+		//refactoring della stringona contenete le esperienze in un jsonArray
+		if (variables.has(VALUTAZIONE_ESPERIENZE_JSON)) {
+			JSONArray esperienze = new JSONArray(variables.getString(VALUTAZIONE_ESPERIENZE_JSON));
+			variables.put(VALUTAZIONE_ESPERIENZE_JSON, esperienze);
+		}
+
+		return variables;
 	}
 }
