07a522ec258a3bf3e737b2c57e14357ece38059e
paoloenricocirone <cironepa@gmail.com>
2018-06-05 14:50:32 +0200

    Issue #109:  - nuova gestione di liquibase: ora in base al profilo spring con cui viene avviata l'applicazione viene caricato un master.xml e dei csv specifici

---
+++ src/main/java/it/cnr/si/config/ServiceRegistryPostProcessor.java
@@ -0,0 +1,41 @@
+package it.cnr.si.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.stereotype.Component;
+
+/**
+ * "Registra" nel registro dei bean la mia definizione customizzata del bean "liquibase"
+ * che "punta" a FlowsDatabaseConfiguration.liquibase() invece di DatabaseConfiguration.liquibase()
+ *
+ * (prende un diverso master a seconda del profilo spring con il quale si avvia l'applicazione)
+ */
+@Component
+public class ServiceRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry)
+            throws BeansException {
+        GenericBeanDefinition gbd = new GenericBeanDefinition();
+
+        gbd.setScope("singleton");
+        gbd.setAbstract(false);
+        gbd.setLazyInit(false);
+        gbd.setAutowireMode(3);
+        gbd.setDependencyCheck(0);
+        gbd.setAutowireCandidate(true);
+        gbd.setPrimary(false);
+        gbd.setFactoryBeanName("flowsDatabaseConfiguration");
+        gbd.setFactoryMethodName("liquibase");
+        gbd.setInitMethodName(null);
+        gbd.setDestroyMethodName("(inferred)");
+
+        registry.registerBeanDefinition("liquibase", gbd );
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {}
+}
