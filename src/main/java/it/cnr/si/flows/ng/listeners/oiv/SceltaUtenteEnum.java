26cf6b65906dcdff475c805c58bb68837bd55291
marco.spasiano <marco.spasiano@cnr.it>
2018-06-12 10:24:10 +0200

    Gestione preavviso di rigetto

---
+++ src/main/java/it/cnr/si/flows/ng/listeners/oiv/SceltaUtenteEnum.java
@@ -0,0 +1,34 @@
+package it.cnr.si.flows.ng.listeners.oiv;
+
+import java.util.Arrays;
+
+public enum SceltaUtenteEnum {
+    PRENDO_IN_CARICO_LA_DOMANDA("prendo_in_carico_la_domanda"),
+    ICHIESTA_SOCCORSO_ISTRUTTORIO("richiesta_soccorso_istruttorio"),
+    INVIO_VALUTAZIONE("invio_valutazione"),
+    RICHIESTA_SOCCORSO_ISTRUTTORIO("richiesta_soccorso_istruttorio"),
+    GENERA_PDF_PREAVVISO_DI_RIGETTO("genera_PDF_preavviso_di_rigetto"),
+    INVIA_PREAVVISO_DI_RIGETTO("invia_preavviso_di_rigetto"),
+    APPROVA("approva"),
+    INVIA_ALLA_VALUTAZIONE("invia_alla_valutazione"),
+    GENERA_PDF_RIGETTO("genera_PDF_rigetto"),
+    INVIA_RIGETTO_FIRMATO("invia_rigetto_firmato");
+
+    private String value;
+
+    SceltaUtenteEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static SceltaUtenteEnum fromValue(String v) {
+        return Arrays.asList(SceltaUtenteEnum.values()).stream()
+                .filter(s -> s.getValue().equals(v))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(v));
+    }
+
+}
