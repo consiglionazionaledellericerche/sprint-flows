acf147805141438b76015dafae50ee81c9b88e7b
paoloenricocirone <cironepa@gmail.com>
2018-06-20 15:04:14 +0200

    Issue #117:  - l'evict della cache sui gruppi e sugli utenti in caso di salvataggio o eliminazione di una relationship

---
+++ src/main/webapp/app/components/links/links.directive.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    angular.module('sprintApp')
+        .directive('links', linksDirective);
+
+    linksDirective.$inject = [];
+
+    function linksDirective() {
+
+        return {
+            restrict: 'E',
+            scope: {
+                links: '='
+            },
+            templateUrl: 'app/components/links/links.html'
+        };
+    }
+})();
