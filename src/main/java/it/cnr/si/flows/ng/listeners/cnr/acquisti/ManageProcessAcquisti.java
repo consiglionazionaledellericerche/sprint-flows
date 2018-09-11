35cfdb5403c182b884050e1bc3b20861729de81b
Massimo <Massimo@150.146.40.130>
2018-09-11 14:50:42 +0200

    sistemato sub flusso contratto fuori mepa

--- src/main/java/it/cnr/si/flows/ng/listeners/cnr/acquisti/ManageProcessAcquisti.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/cnr/acquisti/ManageProcessAcquisti.java
@@ -32,7 +32,7 @@ public class ManageProcessAcquisti implements ExecutionListener {
 	private DittaCandidata dittaCandidata;	
 	@Inject
 	private FlowsProcessInstanceService flowsProcessInstanceService;
-		
+
 	private Expression faseEsecuzione;
 
 	@Override
@@ -175,6 +175,9 @@ public class ManageProcessAcquisti implements ExecutionListener {
 		// START CONTRATTO FUORI MEPA  
 		case "predisposizione-contratto-start": {
 			LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue);
+			if (execution.getVariable("gestioneRTIDittaAggiudicataria").toString().equals("SI")) {
+				dittaCandidata.aggiornaDittaRTICandidata(execution);
+			}
 		};break;     
 		case "predisposizione-contratto-end": {
 			LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue);
@@ -318,7 +321,7 @@ public class ManageProcessAcquisti implements ExecutionListener {
 		case "process-end": {
 			LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue);
 		};break;  
-		
+
 		// DEFAULT  
 		default:  {
 			LOGGER.info("--faseEsecuzione: " + faseEsecuzioneValue);
