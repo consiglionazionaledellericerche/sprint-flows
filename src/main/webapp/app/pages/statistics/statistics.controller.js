2ca24ec7332096d48016cd9f3f0f83f651ef6776
massimo.fraticelli <massimo.fraticelli@cnr.it>
2018-09-24 15:12:34 +0200

    controllo statistiche

--- src/main/webapp/app/pages/statistics/statistics.controller.js
+++ src/main/webapp/app/pages/statistics/statistics.controller.js
@@ -1,23 +1,50 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('sprintApp')
-        .controller('StatisticsController', StatisticsController);
+	angular
+	.module('sprintApp')
+	.controller('StatisticsController', StatisticsController);
 
-    StatisticsController.$inject = ['$scope', '$filter', '$stateParams', 'dataService', 'utils'];
+	StatisticsController.$inject = ['$rootScope', '$scope', '$filter', '$stateParams', 'dataService', 'Principal', 'ProfileService', 'LoginService', 'utils'];
 
-    function StatisticsController($scope, $filter, $stateParams, dataService, utils) {
-        let vm = this,
-            dateFormat = 'yyyy-MM-dd';
-   		vm.processDefinition = $stateParams.processDefinition;
+
+	function StatisticsController($rootScope, $scope, $filter, $stateParams, dataService, Principal, ProfileService, LoginService, utils) {
+		//var vm = this;
+
+
+		let vm = this,
+		dateFormat = 'yyyy-MM-dd';
+		vm.processDefinition = $stateParams.processDefinition.split(":")[0];
+		vm.isNavbarCollapsed = true;
+		vm.isAuthenticated = Principal.isAuthenticated;
 
 		$scope.exportFile = function(isPdf, processDefinitionKey, startDateGreat, startDateLess, filename) {
 			let url = (isPdf ? '/api/makeStatisticPdf?' : '/api/makeStatisticCsv?') +
-			'processDefinitionKey=' + vm.processDefinition.key +
-			 '&startDateGreat=' + $filter('date')(vm.exportParams.startDateGreat, dateFormat) +
-			 '&startDateLess=' + $filter('date')(vm.exportParams.startDateLess, dateFormat);
-            utils.downloadFile(url, filename, isPdf ? 'application/pdf' : 'application/vnd.ms-excel');
-        };
-    }
+			'processDefinitionKey=' + vm.processDefinition +
+			'&startDateGreat=' + $filter('date')(vm.exportParams.startDateGreat, dateFormat) +
+			'&startDateLess=' + $filter('date')(vm.exportParams.startDateLess, dateFormat);
+			utils.downloadFile(url, filename, isPdf ? 'application/pdf' : 'application/vnd.ms-excel');
+		};
+
+		// $scope.strutture = [{id:1,label:"pippo"},{id:2,label:"pluto"}];
+		$scope.strutture = [];
+		var appoStruttura = [];
+		function elencaStrutture() {
+			for (var i = 0; i < Principal.identity().$$state.value.authorities.length; i++){
+				var authority = Principal.identity().$$state.value.authorities[i];
+				if(authority.includes('responsabile#' + vm.processDefinition) || authority.includes('supervisore#' + vm.processDefinition)){
+					var newStruttura = authority.split(/[#@]/)[2];
+					if(newStruttura != null  ){
+						if(appoStruttura.indexOf(newStruttura) == -1) {
+							appoStruttura.push(newStruttura);
+							dataService.lookup.uo(newStruttura).then(function(response){
+								$scope.strutture.push(response.data)
+							})
+						}}
+				}
+			}
+		};
+
+		elencaStrutture();
+	}
 })();
