9de81e385a7636688441b4bec546f138bd11b545
massimo.fraticelli <massimo.fraticelli@cnr.it>
2018-10-02 10:05:52 +0200

    della pubblicazione

--- src/main/java/it/cnr/si/flows/ng/listeners/cnr/acquisti/ManageSceltaUtenteAcquisti.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/cnr/acquisti/ManageSceltaUtenteAcquisti.java
@@ -2,17 +2,21 @@ package it.cnr.si.flows.ng.listeners.cnr.acquisti;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 import org.activiti.engine.delegate.DelegateExecution;
-
+import org.activiti.engine.delegate.Expression;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import it.cnr.si.flows.ng.service.FirmaDocumentoService;
 import it.cnr.si.flows.ng.service.ProtocolloDocumentoService;
+import it.cnr.si.flows.ng.dto.FlowsAttachment;
+import it.cnr.si.flows.ng.service.FlowsAttachmentService;
 
 
 @Service
@@ -24,10 +28,15 @@ public class ManageSceltaUtenteAcquisti {
 	private FirmaDocumentoService firmaDocumentoService;
 	@Inject
 	private ProtocolloDocumentoService protocolloDocumentoService;
+	@Inject
+	private FlowsAttachmentService attachmentService;
+	private Expression nomeFileDaPubblicare;
 
 	public void azioneScelta(DelegateExecution execution, String faseEsecuzioneValue, String sceltaUtente) throws IOException, ParseException {
 		String processInstanceId =  execution.getProcessInstanceId();
 		LOGGER.info("-- azioneScelta: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+		Map<String, FlowsAttachment> attachmentList;
+
 		if (sceltaUtente != null){
 			switch(faseEsecuzioneValue){  
 			case "firma-decisione-end": {
@@ -77,7 +86,162 @@ public class ManageSceltaUtenteAcquisti {
 					LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
 					protocolloDocumentoService.protocolla(execution, "provvedimentoRevoca");
 				}
-			};break;			  
+			};break;
+
+
+			case "DECISIONE-CONTRATTARE-end": {
+				LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+				attachmentList = attachmentService.getAttachementsForProcessInstance(processInstanceId);
+				if(sceltaUtente.equals("RevocaSemplice")) {
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						LOGGER.info("Key = " + key + ", Value = " + value);
+						attachmentService.setPubblicabile(execution.getId(), value.getName(), false);					
+					}
+				} else {					
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						if	(value != null && ( value.getName().startsWith("allegatiPubblicabili") 
+								|| value.getName().equals("decisioneContrattare"))){
+							attachmentService.setPubblicabile(execution.getId(), value.getName(), true);					
+						}
+					}
+				}
+			};break;
+			
+			case "PROVVEDIMENTO-AGGIUDICAZIONE-start": {
+				LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+				
+				attachmentService.setPubblicabile(execution.getId(), "giustificazioniAnomalie", true);
+				attachmentService.setPubblicabile(execution.getId(), "provvedimentoNominaCommissione", true);
+				attachmentService.setPubblicabile(execution.getId(), "provvedimentoAmmessiEsclusi", true);
+				attachmentService.setPubblicabile(execution.getId(), "esitoValutazioneAnomalie", true);
+				attachmentService.setPubblicabile(execution.getId(), "elencoDitteCandidate", true);
+				attachmentService.setPubblicabile(execution.getId(), "elencoVerbali", true);
+				
+				// TODO aggiungere anche gli array bandoAvvisi letteraInvito allegatiPubblicabili
+				
+			};break;	
+
+			case "PROVVEDIMENTO-AGGIUDICAZIONE-end": {
+				LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+				attachmentList = attachmentService.getAttachementsForProcessInstance(processInstanceId);
+				if(sceltaUtente.equals("RevocaConProvvedimento")) {
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						LOGGER.info("Key = " + key + ", Value = " + value);
+						attachmentService.setPubblicabile(execution.getId(), value.getName(), false);					
+					}
+				} else {					
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						if	(value != null && ( value.getName().startsWith("allegatiPubblicabili")
+								 || value.getName().equals("provvedimentoAggiudicazione"))){
+							attachmentService.setPubblicabile(execution.getId(), value.getName(), true);					
+						}
+					}
+				}
+			};break;	
+
+
+			case "CONTRATTO-FUORI-MEPA-start": {
+				LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+				attachmentList = attachmentService.getAttachementsForProcessInstance(processInstanceId);		
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						if	(value != null && ( value.getName().startsWith("allegatiPubblicabili") 
+								 || value.getName().equals("bandoAvvisi")
+								 || value.getName().equals("letteraInvito")
+								 )){
+							attachmentService.setPubblicabile(execution.getId(), value.getName(), true);					
+						}
+				}
+			};break;	
+			
+			
+			case "CONTRATTO-FUORI-MEPA-end": {
+				LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+				attachmentList = attachmentService.getAttachementsForProcessInstance(processInstanceId);
+				if(sceltaUtente.equals("RevocaConProvvedimento")) {
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						LOGGER.info("Key = " + key + ", Value = " + value);
+						attachmentService.setPubblicabile(execution.getId(), value.getName(), false);					
+					}
+				} else {					
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						if	(value != null && ( value.getName().startsWith("allegatiPubblicabili") 
+								 || value.getName().equals("contratto")
+								 )){
+							attachmentService.setPubblicabile(execution.getId(), value.getName(), true);					
+						}
+					}
+				}
+			};break;
+
+
+
+			case "STIPULA-MEPA-start": {
+				LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+				attachmentList = attachmentService.getAttachementsForProcessInstance(processInstanceId);		
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						if	(value != null && ( value.getName().startsWith("allegatiPubblicabili") 
+								 || value.getName().equals("bandoAvvisi")
+								 || value.getName().equals("letteraInvito")
+								 )){
+							attachmentService.setPubblicabile(execution.getId(), value.getName(), true);					
+						}
+				}
+			};break;	
+			
+			case "STIPULA-MEPA-end": {
+				LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+				attachmentList = attachmentService.getAttachementsForProcessInstance(processInstanceId);
+				if(sceltaUtente.equals("RevocaConProvvedimento")) {
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						LOGGER.info("Key = " + key + ", Value = " + value);
+						attachmentService.setPubblicabile(execution.getId(), value.getName(), false);					
+					}
+				} else {					
+					for (String key : attachmentList.keySet()) {
+						FlowsAttachment value = attachmentList.get(key);
+						if	(value != null && ( value.getName().startsWith("allegatiPubblicabili")
+								|| value.getName().equals("stipula"))){
+							attachmentService.setPubblicabile(execution.getId(), value.getName(), true);					
+						}
+					}
+				}
+			};break;		
+			
+
+			case "REVOCA-end": {
+				LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+				attachmentList = attachmentService.getAttachementsForProcessInstance(processInstanceId);
+				for (String key : attachmentList.keySet()) {
+					FlowsAttachment value = attachmentList.get(key);
+					LOGGER.info("Key = " + key + ", Value = " + value);
+					attachmentService.setPubblicabile(execution.getId(), value.getName(), false);					
+				}
+
+			};break;	
+
+			case "consuntivo-end": {
+				LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+				attachmentList = attachmentService.getAttachementsForProcessInstance(processInstanceId);
+				for (String key : attachmentList.keySet()) {
+					FlowsAttachment value = attachmentList.get(key);
+					if	(value != null && ( value.getName().startsWith("allegatiPubblicabili")
+							|| value.getName().equals("avvisoPostInformazione")
+							|| value.getName().equals("modificheVariantiArt106"))){
+						attachmentService.setPubblicabile(execution.getId(), value.getName(), true);					
+					}
+				}
+			};break;			
+
+
 			case "firma-revoca-end": {
 				if(sceltaUtente.equals("Firma")) {
 					LOGGER.info("-- faseEsecuzione: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
