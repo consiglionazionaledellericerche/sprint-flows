fdd07641a1dadaa2d542feae3a244886cecb3153
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2018-11-20 15:12:49 +0100

    fileinput di nuova generazione

--- src/main/webapp/app/inputs/fileinput/fileinput.directive.js
+++ src/main/webapp/app/inputs/fileinput/fileinput.directive.js
@@ -20,25 +20,73 @@
         return {
             restrict: 'E',
             templateUrl: 'app/inputs/fileinput/fileinput.html',
-            scope: true,
+            scope: {
+                pubblicazioneUrp: '@?',
+                pubblicazioneTrasparenza: '@?',
+                protocollo: '@?',
+                metadatiPubblicazione: '@?',
+                metadatiProtocollo: '@?',
+                multiple: '@?',
+                name: '@?',
+                label: '@?',
+                legend: '@?'
+            },
             link: function ($scope, element, attrs) {
-                $scope.attrs = attrs;
-                $scope.model = attrs.model;
 
-                if ($scope.$parent.attachments === undefined)
-                    $scope.$parent.attachments = [];
+                function init() {
+                    $scope.min = $scope.min || 0;
+                    $scope.max = $scope.max || 999;
 
-                if ($scope.attrs.multiple == 'false')
-                    $scope.document = $scope.$parent.attachments.find(function(el) {
-                        return el.name === $scope.attrs.name;
-                    })
+                    $scope.data = $scope.$parent.data;
 
+                    $scope.metadatiPubblicazione = ($scope.metadatiPubblicazione == 'true');
+                    $scope.pubblicazioneUrp = ($scope.pubblicazioneUrp == 'true');
+                    $scope.pubblicazioneTrasparenza = ($scope.pubblicazioneTrasparenza == 'true');
+                    $scope.metadatiProtocollo = ($scope.metadatiProtocollo == 'true');
+                    $scope.protocollo = ($scope.protocollo == 'true');
+
+                    $scope.scope = $scope;
+                    $scope.attrs = attrs;
+
+                    $scope.rows = $scope.name ? [{}] : [];
+
+                    if ($scope.$parent.attachments === undefined)
+                        $scope.$parent.attachments = [];
+
+                    if ($scope.multiple == 'false')
+                        $scope.document = $scope.$parent.attachments.find(function(el) {
+                            return el.name === $scope.attrs.name;
+                        })
+                 }
+
+                init();
 
                 $scope.filterNames = function(value) {
                     var reg = "^"+$scope.attrs.name+"\\[\\d+\\]";
                     var tester = new RegExp(reg, 'g');
                     return tester.test(value.name);
                 }
+
+                $scope.addRow = function() {
+                    if ($scope.rows.length < $scope.max)
+                        $scope.rows.push({});
+                    return false;
+                };
+                $scope.removeRow = function() {
+                    if ($scope.rows.length > $scope.min)
+                        $scope.rows.pop();
+
+                    // elimino i dati dell'ultimo alloegato aggiunto
+                    var name = 'allegato'+$scope.rows.length;
+                    delete $scope.$parent.data[name];
+                    delete $scope.$parent.data[name+'_name'];
+                    delete $scope.$parent.data[name+'_pubblicazioneUrp'];
+                    delete $scope.$parent.data[name+'_pubblicazioneTrasparenza'];
+                    delete $scope.$parent.data[name+'_protocollo'];
+                    delete $scope.$parent.data[name+'_dataprotocollo'];
+                    delete $scope.$parent.data[name+'_numeroprotocollo'];
+                    return false;
+                };
 
             }
         }
