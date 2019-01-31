7b71aa25df36f90928ff71575cbb91cb64d24caf
paoloenricocirone <cironepa@gmail.com>
2019-01-31 14:57:00 +0100

    todo da ricordarsi per il refactoring

--- src/main/webapp/app/components/form/pagination.constants.js
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -4,6 +4,6 @@
     angular
         .module('sprintApp')
         .constant('paginationConstants', {
-            'itemsPerPage': 20
+            'itemsPerPage': 20 //todo: refactoring itemsPerPage => itemsForPage
         });
 })();
