61e36f8c8626c61b241843ecdd3fb9ae45d1644f
paoloenricocirone <cironepa@gmail.com>
2019-02-04 15:06:16 +0100

    Fix pagine di ricerca

--- src/main/webapp/app/pages/search/search.controller.js
+++ src/main/webapp/app/pages/search/search.controller.js
@@ -1,103 +1,114 @@
 (function() {
-	'use strict';
+  "use strict";
 
-	angular
-		.module('sprintApp')
-		.controller('SearchController', SearchController);
+  angular.module("sprintApp").controller("SearchController", SearchController);
 
-	SearchController.$inject = ['$scope', 'dataService', 'utils', '$log', '$location'];
+  SearchController.$inject = ["$scope", "dataService", "utils", "$log", "$location"];
 
-	function SearchController($scope, dataService, utils, $log, $location) {
-		var vm = this,
-			oldUrl = $scope.formUrl;
+  function SearchController($scope, dataService, utils, $log, $location) {
+    var vm = this,
+      oldUrl = $scope.formUrl;
 
-		$scope.reload = false;
+    $scope.reload = false;
 
-		vm.searchParams = $location.search();
-		vm.searchParams.active = $location.search().active || true;
-		vm.searchParams.order = $location.search().order || 'ASC';
-		vm.searchParams.page = $location.search().page || 1;
-		vm.searchParams.processDefinitionKey = 'all';
-		
-		if ($location.search().isTaskQuery === undefined){
-			vm.searchParams.isTaskQuery = false;
-		}else{
-			vm.searchParams.isTaskQuery = ($location.search().isTaskQuery === true || $location.search().isTaskQuery == 'true');
-		}
+    vm.searchParams = $location.search();
+    vm.searchParams.active = $location.search().active || true;
+    vm.searchParams.order = $location.search().order || "ASC";
+    vm.searchParams.page = $location.search().page || 1;
+    vm.searchParams.processDefinitionKey = "all";
 
-		$scope.hasPendingRequests = function() {
-			return httpRequestTracker.hasPendingRequests();
-		};
+    if ($location.search().isTaskQuery === undefined) {
+      vm.searchParams.isTaskQuery = false;
+    } else {
+      vm.searchParams.isTaskQuery =
+        $location.search().isTaskQuery === true ||
+        $location.search().isTaskQuery == "true";
+    }
 
-		$scope.search = function() {
-			//serve per evitare di ricaricare le form di ricerca associate alla Process Definition ad ogni nuova ricerca
-			$scope.reload = $scope.formUrl !== oldUrl;
 
-			vm.results = [];
-			vm.loading = true;
+    $scope.search = function() {
+      //serve per evitare di ricaricare le form di ricerca associate alla Process Definition ad ogni nuova ricerca
+      $scope.reload = $scope.formUrl !== oldUrl;
 
-			if (vm.searchParams.processDefinitionKey === null)
-				vm.searchParams.processDefinitionKey = undefined;
+      vm.results = [];
+      vm.loading = true;
 
-			$log.info(vm.searchParams);
+      if (vm.searchParams.processDefinitionKey === null)
+        vm.searchParams.processDefinitionKey = undefined;
 
-			$location.search(vm.searchParams);
+      $log.info(vm.searchParams);
 
-			//ripulisco i valori con null(tipo quando annullo la data selezionata)
-			Object.keys(vm.searchParams).forEach((key) => (vm.searchParams[key] == null) && delete vm.searchParams[key]);
-			dataService.processInstances.search(vm.searchParams)
-				.then(function(response) {
-					if (vm.searchParams.isTaskQuery)
-						vm.results = utils.refactoringVariables(response.data.tasks);
-					else
-						vm.results = utils.refactoringVariables(response.data.processInstances);
+      $location.search(vm.searchParams);
 
-					vm.totalItems = response.data.totalItems;
-					vm.loading = false;
+      //ripulisco i valori con null(tipo quando annullo la data selezionata)
+      Object.keys(vm.searchParams).forEach(
+        key => vm.searchParams[key] == null && delete vm.searchParams[key]
+      );
+      dataService.processInstances.search(vm.searchParams).then(
+        function(response) {
+          vm.results = utils.refactoringVariables(response.data.data);
 
-				}, function(response) {
-					$log.error(response);
-					vm.loading = false;
-				});
-		};
+          vm.totalItems = response.data.total;
+          vm.loading = false;
+        },
+        function(response) {
+          $log.error(response);
+          vm.loading = false;
+        }
+      );
+    };
 
-		$scope.stripParams = function() {
-			var cleanParams = {};
-			cleanParams.active = vm.searchParams.active;
-			cleanParams.order = vm.searchParams.order;
-			cleanParams.page = 1;
-			cleanParams.isTaskQuery = vm.searchParams.isTaskQuery;
-			cleanParams.processDefinitionKey = vm.searchParams.processDefinitionKey;
-			cleanParams.numeriProtocollo = vm.searchParams.numeriProtocollo;
 
-			vm.searchParams = cleanParams;
-		};
+    $scope.exportCsv = function() {
+      dataService.search
+        .exportCsv(vm.searchParams, -1, -1)
+        .success(function(response) {
+          var filename = new Date().toISOString().slice(0, 10) + ".xls",
+            file = new Blob([response], {
+              type: "application/vnd.ms-excel"
+            });
+          $log.info(file, filename);
+          saveAs(file, filename);
+        });
+    };
 
-		$scope.exportCsv = function() {
-			dataService.search.exportCsv(vm.searchParams, -1, -1)
-				.success(function(response) {
-					var filename = new Date().toISOString().slice(0, 10) + '.xls',
-						file = new Blob([response], {
-							type: 'application/vnd.ms-excel'
-						});
-					$log.info(file, filename);
-					saveAs(file, filename);
-				});
-		};
 
-		$scope.$watchGroup(['vm.searchParams.processDefinitionKey'], function() {
+    $scope.$watchGroup(["vm.searchParams.isTaskQuery"], function() {
+      var cleanParams = {};
+      cleanParams.active = vm.searchParams.active;
+      cleanParams.order = vm.searchParams.order;
+      cleanParams.page = 1;
+      cleanParams.isTaskQuery = vm.searchParams.isTaskQuery;
+      cleanParams.processDefinitionKey = vm.searchParams.processDefinitionKey;
+      cleanParams.numeriProtocollo = vm.searchParams.numeriProtocollo;
 
-			if (vm.searchParams.processDefinitionKey) {
-				if (vm.searchParams.isTaskQuery) {
-					$scope.formUrl = 'api/forms/' + vm.searchParams.processDefinitionKey + '/1/search-ti';
-				} else {
-					$scope.formUrl = 'api/forms/' + vm.searchParams.processDefinitionKey + '/1/search-pi';
-				}
-			} else{
-				$scope.formUrl = undefined;
-			}
-		});
+      vm.searchParams = cleanParams;
+      $scope.search();
+    });
 
-		$scope.search();
-	}
+
+    $scope.$watchGroup(
+      ["vm.searchParams.processDefinitionKey", "vm.searchParams.isTaskQuery"],
+      function() {
+        if (vm.searchParams.processDefinitionKey) {
+          if (vm.searchParams.isTaskQuery) {
+            $scope.formUrl =
+              "api/forms/" +
+              vm.searchParams.processDefinitionKey +
+              "/1/search-ti";
+          } else {
+            $scope.formUrl =
+              "api/forms/" +
+              vm.searchParams.processDefinitionKey +
+              "/1/search-pi";
+          }
+        } else {
+          $scope.formUrl = undefined;
+        }
+      }
+    );
+
+
+    $scope.search();
+  }
 })();
