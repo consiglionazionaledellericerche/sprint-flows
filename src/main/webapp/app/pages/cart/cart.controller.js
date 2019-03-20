2b0303b2f6f4a99883749fe02a5767b45c46b4bc
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2019-03-20 11:12:27 +0100

    WIP: carrello

--- src/main/webapp/app/pages/cart/cart.controller.js
+++ src/main/webapp/app/pages/cart/cart.controller.js
@@ -11,5 +11,23 @@
         var vm = this;
 
     }
+    
+    
+    angular.module('aprintApp').factory('cartService', cartService);
+    
+    cartService.$inject = ['$rootScope']
+    
+    function cartService($rootScope) {
+    	
+    	return {
+    		addToCart: function(taskId) {
+    			$rootScope.cart = $rootScope.cart || [];
+    			$rootScope.cart[taskId] = taskId;
+    		},
+    		clearCart: function() {
+    			$rootScope.cart = {};
+    		}
+    	}
+    }
 
 })();
