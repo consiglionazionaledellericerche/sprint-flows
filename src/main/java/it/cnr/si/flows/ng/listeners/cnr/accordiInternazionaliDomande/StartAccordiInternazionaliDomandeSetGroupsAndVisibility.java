ae95720438d16787aac69f46952918dd86d14584
massimo.fraticelli <massimo.fraticelli@cnr.it>
2019-04-30 17:53:09 +0200

    gestione print pdf valutazioneProgettoAccordiBilaterali

--- src/main/java/it/cnr/si/flows/ng/listeners/cnr/accordiInternazionaliDomande/StartAccordiInternazionaliDomandeSetGroupsAndVisibility.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/cnr/accordiInternazionaliDomande/StartAccordiInternazionaliDomandeSetGroupsAndVisibility.java
@@ -1,6 +1,7 @@
 package it.cnr.si.flows.ng.listeners.cnr.accordiInternazionaliDomande;
 
 
+import it.cnr.si.flows.ng.service.AceBridgeService;
 import it.cnr.si.flows.ng.utils.Enum;
 
 import org.activiti.engine.RuntimeService;
@@ -15,6 +16,7 @@ import javax.inject.Inject;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.List;
 
 import static it.cnr.si.flows.ng.utils.Utils.PROCESS_VISUALIZER;
 
@@ -27,13 +29,19 @@ public class StartAccordiInternazionaliDomandeSetGroupsAndVisibility {
 
 	@Inject
 	private RuntimeService runtimeService;
-
+	@Inject
+	private AceBridgeService aceBridgeService;
+	
 	public void configuraVariabiliStart(DelegateExecution execution)  throws IOException, ParseException  {
 
 		String initiator = (String) execution.getVariable(Enum.VariableEnum.initiator.name());
 		// LOGGER.info("L'utente {} sta avviando il flusso {} (con titolo {})", initiator, execution.getId(), execution.getVariable(Enum.VariableEnum.title.name()));
 		LOGGER.info("L'utente {} sta avviando il flusso {} (con titolo {})", initiator, execution.getId(), execution.getVariable("title"));
-
+		List<String> gruppiUtente = aceBridgeService.getAceGroupsForUser(execution.getVariable("userNameRichiedente").toString());
+		gruppiUtente.forEach(gruppoUtente -> {
+			execution.setVariable("strutturaAppartenenzaUtente", gruppoUtente.toString());
+		});
+		
 		String gruppoValidatoriAccordiInternazionali = "validatoriAccordiInternazionali@0000";
 		String gruppoUfficioProtocollo = "ufficioProtocolloAccordiInternazionali@0000";
 		String gruppoValutatoreScientificoDipartimento = "valutatoreScientificoDipartimento@0000";
@@ -51,6 +59,7 @@ public class StartAccordiInternazionaliDomandeSetGroupsAndVisibility {
 		runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoUfficioProtocollo, PROCESS_VISUALIZER);
 		runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoDirigenteRichiedente, PROCESS_VISUALIZER);
 		runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), gruppoValutatoreScientificoDipartimento, PROCESS_VISUALIZER);
+		runtimeService.addGroupIdentityLink(execution.getProcessInstanceId(), applicazioneScrivaniaDigitale, PROCESS_VISUALIZER);
 
 		execution.setVariable("gruppoValidatoriAccordiInternazionali", gruppoValidatoriAccordiInternazionali);
 		execution.setVariable("gruppoResponsabileAccordiInternazionali", gruppoResponsabileAccordiInternazionali);
