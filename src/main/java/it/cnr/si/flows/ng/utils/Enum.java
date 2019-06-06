d6447030f5a3bb48e5b2492c825b48e4fd4019a1
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2019-06-06 10:21:24 +0200

    Modifica per accettare files da altre applicazioni via nodeRef

--- src/main/java/it/cnr/si/flows/ng/utils/Enum.java
+++ src/main/java/it/cnr/si/flows/ng/utils/Enum.java
@@ -72,7 +72,8 @@ public class Enum {
         RimozioneDaPubblicazioneTrasparenza,
         RimozioneDaPubblicazioneUrp,
         SostituzioneProtocollo,
-        RettificaProtocollo
+        RettificaProtocollo,
+        linkDaAltraApplicazione
     }
 
 
