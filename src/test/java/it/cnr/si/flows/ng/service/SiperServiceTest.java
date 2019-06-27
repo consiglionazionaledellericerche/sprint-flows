2445441ad5530b97916edbbf18614ba60f519973
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2019-06-27 15:07:30 +0200

    Test funzione Siper

---
+++ src/test/java/it/cnr/si/flows/ng/service/SiperServiceTest.java
@@ -0,0 +1,29 @@
+package it.cnr.si.flows.ng.service;
+
+import it.cnr.si.FlowsApp;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.inject.Inject;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FlowsApp.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableTransactionManagement
+@ActiveProfiles("test,cnr")
+public class SiperServiceTest {
+
+    @Inject
+    private SiperService siperService;
+
+    @Test
+    public void testGetResponsabileUo() {
+
+        System.out.println(siperService.getResponsabileCDSUO("000400").get(0).get("codice_sede"));
+
+
+    }
+}
