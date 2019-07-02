4f8c5a5ee099d11a001888dcba39e515606d8cc5
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2019-07-02 10:19:24 +0200

    Prova incremento maxPoolSize

--- src/main/java/it/cnr/si/flows/ng/config/MailConfguration.java
+++ src/main/java/it/cnr/si/flows/ng/config/MailConfguration.java
@@ -1,42 +1,80 @@
 package it.cnr.si.flows.ng.config;
 
-import java.util.List;
-
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ReplicatedMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.util.List;
+
 @Configuration
 public class MailConfguration {
 
+    private static final Logger log = LoggerFactory.getLogger(MailConfguration.class);
+
+    public static final String MAIL_CONFIG = "mailConfig";
+    public static final String MAIL_ACTIVATED = "mailActivated";
+    public static final String MAIL_RECIPIENTS = "mailRecipients";
+
+
     @Value("${spring.mail.recipients:}")
-    private List<String> mailRecipients;
+    private List<String> initMailRecipients;
     @Value("${spring.mail.activated:false}")
-    private boolean mailActivated;
+    private boolean initMailActivated;
+
+    @Inject
+    private HazelcastInstance hazelcastInstance;
+
+    @Scheduled(fixedDelay = 3000, initialDelay = 10000) // 10m
+    public void logStatus() {
+        log.debug("MailConfig: {} {}", isMailActivated(), getMailRecipients());
+    }
+
+
+    @PostConstruct
+    public void init() {
+        ReplicatedMap<Object, Object> mailConfig = hazelcastInstance.getReplicatedMap(MAIL_CONFIG);
+
+        if (!mailConfig.containsKey(MAIL_ACTIVATED)) {
+            log.info("MailConfig non settato, imposto i predefiniti: {} {}", initMailActivated, initMailRecipients);
+
+            mailConfig.put(MAIL_ACTIVATED, initMailActivated);
+            mailConfig.put(MAIL_RECIPIENTS, initMailRecipients);
+
+        } else {
+            log.info("MailConfig gia' settato, leggo le impostazioni predefiniti: {} {}", mailConfig.get(MAIL_ACTIVATED), MAIL_RECIPIENTS);
+        }
+    }
 
     public List<String> getMailRecipients() {
-        return mailRecipients;
+        return (List<String>) hazelcastInstance.getReplicatedMap(MAIL_CONFIG).get(MAIL_RECIPIENTS);
     }
     public void setMailRecipients(List<String> mailRecipients) {
-        this.mailRecipients = mailRecipients;
+        hazelcastInstance.getReplicatedMap(MAIL_CONFIG).put(MAIL_RECIPIENTS, mailRecipients);
     }
 
     public boolean isMailActivated() {
-        return mailActivated;
+        return (boolean) hazelcastInstance.getReplicatedMap(MAIL_CONFIG).get(MAIL_ACTIVATED);
     }
     public void setMailActivated(boolean mailActivated) {
-        this.mailActivated = mailActivated;
+        hazelcastInstance.getReplicatedMap(MAIL_CONFIG).put(MAIL_ACTIVATED, mailActivated);
     }
 
     // --- //
 
     private ITemplateResolver htmlTemplateResolver() {
         final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setOrder(Integer.valueOf(2));
+        templateResolver.setOrder(2);
         templateResolver.setPrefix("/mails/");
 //        templateResolver.setResolvablePatterns(Collections.singleton("html/*"));
         templateResolver.setSuffix(".html");
