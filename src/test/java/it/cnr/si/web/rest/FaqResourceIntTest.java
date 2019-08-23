9108a15b21d7c0e640b44d92745ba8625fbaa7b2
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2019-08-23 13:45:33 +0200

    ristrutturazione test

--- src/test/java/it/cnr/si/web/rest/FaqResourceIntTest.java
+++ src/test/java/it/cnr/si/web/rest/FaqResourceIntTest.java
@@ -1,23 +1,18 @@
 package it.cnr.si.web.rest;
 
 import it.cnr.si.FlowsApp;
-import it.cnr.si.FlowsTest;
-import it.cnr.si.SprintApp;
 import it.cnr.si.domain.Faq;
 import it.cnr.si.flows.ng.TestUtil;
 import it.cnr.si.repository.FaqRepository;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.hamcrest.Matchers.hasItem;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
@@ -31,6 +26,7 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
