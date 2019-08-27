1790de97cccd867fa7e45d285d9e594045b5defe
massimo.fraticelli <massimo.fraticelli@cnr.it>
2019-08-27 14:03:43 +0200

    ripristino ricalcolo fasci

--- src/main/java/it/cnr/si/flows/ng/listeners/oiv/ManageProcessIscrizioneElencoOiv.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/oiv/ManageProcessIscrizioneElencoOiv.java
@@ -168,9 +168,9 @@ public class ManageProcessIscrizioneElencoOiv implements ExecutionListener {
 			LOGGER.info("VALUTAZIONE_END");
 			LOGGER.info("PER CANCELLAZIONE");
 			String valSceltaUtente = execution.getVariable("sceltaUtente").toString();
-			//if((valSceltaUtente.equals("genera_PDF_preavviso_di_rigetto") || valSceltaUtente.equals("calcola_punteggio") || valSceltaUtente.equals("aggiorna_PDF_preavviso_di_rigetto"))) {
-			//	calcolaPunteggioFascia.calcolaAggiornaGiudizioFinale(execution, nonAggiornaGiudizioFinale);
-			//}
+			if((valSceltaUtente.equals("genera_PDF_preavviso_di_rigetto") || valSceltaUtente.equals("calcola_punteggio") || valSceltaUtente.equals("aggiorna_PDF_preavviso_di_rigetto"))) {
+				calcolaPunteggioFascia.calcolaAggiornaGiudizioFinale(execution, nonAggiornaGiudizioFinale);
+			}
 			// TODO rest per calcolo fascia 
 			//application-fp/applications-ricalcola-fascia?applicationId={nodeRef}
 		}
