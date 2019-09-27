dee1cdeb3682b28ca26cdeacf7ee7c05850dc2b0
paoloenricocirone <cironepa@gmail.com>
2019-09-27 11:06:15 +0200

    Servizi di esportazione delle Pi:   -  rimozione @Secured(AuthoritiesConstants.ADMIN)

--- src/main/java/it/cnr/si/flows/ng/resource/FlowsProcessInstanceResource.java
+++ src/main/java/it/cnr/si/flows/ng/resource/FlowsProcessInstanceResource.java
@@ -213,14 +213,13 @@ public class FlowsProcessInstanceResource {
      * @param order             l`ordine (ASC o DESC) in base alla data di start del flusso (non richiesto, può anche essere nullo)
      * @return le process instances da esportare in trasparenza
      */
-    @PostMapping(value = "/getProcessInstancesForTrasparenza", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Secured(AuthoritiesConstants.ADMIN)
-    @PreAuthorize("hasRole('ROLE_ADMIN') OR hasRole('ROLE_applicazione-portalecnr@0000')")
+    @GetMapping(value = "/getProcessInstancesForTrasparenza", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_applicazione-portalecnr@0000')")
     @Timed
     public ResponseEntity<List<Map<String, Object>>> getProcessInstancesForTrasparenza(
             @RequestParam("firstResult") int firstResult,
             @RequestParam("maxResults") int maxResults,
-            @RequestParam(name = "order", required = false) String order) throws ParseException {
+            @RequestParam(name = "order", required = false) String order) {
 
         List<HistoricProcessInstance> historicProcessInstances =
                 flowsProcessInstanceService.getProcessInstancesForTrasparenza(firstResult, maxResults, order);
@@ -229,9 +228,8 @@ public class FlowsProcessInstanceResource {
     }
 
 
-    @PostMapping(value = "/getProcessInstancesForURP", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Secured(AuthoritiesConstants.ADMIN)
-    @PreAuthorize("hasRole('ROLE_ADMIN') OR hasRole('ROLE_applicazione-portalecnr@0000')")
+    @GetMapping(value = "/getProcessInstancesForURP", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_applicazione-portalecnr@0000')")
     @Timed
     public ResponseEntity<List<Map<String, Object>>> getProcessInstancesForURP(
             @RequestParam("terminiRicorso") int terminiRicorso,
