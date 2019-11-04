f70ab94e4947acd0e29c531972ebf97d4630d45d
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2019-11-04 15:37:14 +0100

    Aggiustamenti dipendenze

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@
 mvn clean spring-boot:run \
 	-Dserver.port=8080 \
 	-Dspring.profiles.active=demo,oiv,swagger \
-	-Dspring.datasource.url= \ # ex. jdbc:postgresql://localhost:5432/flows
-	-Dspring.datasource.username=activiti
-	-Dspring.datasource.password=activitipw 
+	-Dspring.datasource.url= \ # es. jdbc:postgresql://localhost:5432/flows
+	-Dspring.datasource.username=activiti \
+	-Dspring.datasource.password=activitipw \
+	-Doiv.baseurl=http://elenco-oiv-test.si.cnr.it \
+	-Doiv.psw= \
+	-Doiv.mail.mail.user= \
+	-Doiv.mail.mail.password=
