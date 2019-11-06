71f40fe6671bb1f3415b54dd72dfbb3e3b46ae40
TRYCZ MARCINIRENEUSZ <marcinireneusz.trycz@cnr.it>
2019-11-06 09:38:36 +0000

    FlowsTaskResource:  - assignTask: service di assegnazione del task ad un utente specifico (TODO: fare i test e vedere chi può assegnare un task)  - completeTask: aggiunta dell'identityLink associato all'utente che completa il task ("esecutore")  - taskCompleted: service di ricerca dei task completati dall'utente FlowsTaskResourceTest  - test del service taskCompleted

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
 
-mvn clean spring-boot:run -Dserver.port=8082
+mvn clean spring-boot:run \
+	-Dserver.port=8080 \
+	-Dspring.profiles.active=demo,oiv,swagger \
+	-Dspring.datasource.url= \ # es. jdbc:postgresql://localhost:5432/flows
+	-Dspring.datasource.username=activiti \
+	-Dspring.datasource.password=activitipw \
+	-Doiv.baseurl=http://elenco-oiv-test.si.cnr.it \
+	-Doiv.psw= \
+	-Doiv.mail.mail.user= \
+	-Doiv.mail.mail.password=
