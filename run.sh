3904ca8aef2f31326033f890636bca4d3a201689
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2019-12-03 17:42:38 +0100

    docker script

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-mvn clean spring-boot:run -Pprod -DskipTests -Dspring.profiles.active=native,showcase,test,swagger
+mvn clean compile package spring-boot:run -Pprod -DskipTests -Dspring.profiles.active=native,showcase,test,swagger
+
+cp ./target/*.war ./target/app.war
+
+docker build --file src/main/docker/test/Dockerfile -t docker.si.cnr.it/sprint-flows-showcase .
+
+docker run docker.si.cnr.it/sprint-flows-showcase
+
 
