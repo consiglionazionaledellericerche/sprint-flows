bce21a53a8f3a3237c924dfb407771a5617b2a0e
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2019-12-04 11:06:20 +0100

    build e docker funzionano out-of-the-box

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-mvn clean compile package spring-boot:run -Pprod -DskipTests -Dspring.profiles.active=native,showcase,test,swagger
+mvn clean compile package -Pprod -DskipTests -Dspring.profiles.active=native,showcase,test,swagger
 
 cp ./target/*.war ./target/app.war
 
 docker build --file src/main/docker/showcase/Dockerfile -t docker.si.cnr.it/sprint-flows-showcase .
 
-docker run docker.si.cnr.it/sprint-flows-showcase
+docker run -p 8080:8080 docker.si.cnr.it/sprint-flows-showcase
 
 
