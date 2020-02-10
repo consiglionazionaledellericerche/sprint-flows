8f9d88c9f22446a48e4ea1ebdc98d2fc505a575d
Marcin Ireneusz Trycz <marcinireneusz.trycz@cnr.it>
2020-02-10 15:15:54 +0100

    Istruzioni per l'uso

--- run-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-mvn clean spring-boot:run \
-	-Dserver.port=8080 \
-	-Dspring.profiles.active=dev,oiv,swagger 
--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 
-mvn clean spring-boot:run \
-	-Dserver.port=8080 \
-	-Dspring.profiles.active=demo,oiv,swagger \
-	-Dspring.datasource.url= \ # es. jdbc:postgresql://localhost:5432/flows
-	-Dspring.datasource.username=activiti \
-	-Dspring.datasource.password=activitipw \
-	-Doiv.baseurl=http://elenco-oiv-test.si.cnr.it \
-	-Doiv.psw= \
-	-Doiv.mail.mail.user= \
-	-Doiv.mail.mail.password=
+mvn clean compile package -Pprod -DskipTests
+java -jar target/sprint-flows-1.0.30-SNAPSHOT.war --spring.profiles.active=dev,oiv,swagger
+
+# se e' disponibile un database postgres usare invece:
+
+#java -jar target/sprint-flows-1.0.30-SNAPSHOT.war \
+#  --spring.profiles.active=demo,oiv,swagger \
+#	-Dspring.datasource.url=jdbc:postgresql://localhost:5432/flows \ # es. jdbc:postgresql://localhost:5432/flows
+#	-Dspring.datasource.username=activiti \
+#	-Dspring.datasource.password=activitipw \
+#	-Doiv.baseurl=http://elenco-oiv-test.si.cnr.it \
+#	-Doiv.psw= \
+#	-Doiv.mail.mail.user= \
+#	-Doiv.mail.mail.password=
