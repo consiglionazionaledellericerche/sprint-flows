34833c8ba3ad32e949f5e843274d1b856b4efec9
martin <martin@LAPTOP-00AACAV2>
2020-04-15 19:29:47 +0200

    componente wysiwyg

---
+++ src/main/webapp/app/inputs/wysiwyg/wysiwyg.directive.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+
+    angular.module('sprintApp')
+        .directive('wysiwyg', wysiwyg);
+
+    wysiwyg.$inject = ['dataService', '$log'];
+
+    /**
+     * Questa direttiva e' un semplicissimo typeahead con dei parametri preimpostati
+     * L'API e' semplicissima: ng-model e ng-required
+     */
+    function wysiwyg(dataService, $log) {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/inputs/wysiwyg/wysiwyg.html',
+            scope: {
+                ngModel: '=',
+                ngRequired: '@'
+            },
+            link: function($scope, element, attrs) {
+            	
+            	// workaround necessario se ci sono piu' editor sulla stessa pagina
+            	// bug: angular non aggiorna le classi css
+            	var events = ['trixInitialize', 'trixChange', 'trixSelectionChange', 'trixFocus', 'trixBlur', 'trixFileAccept', 'trixAttachmentAdd', 'trixAttachmentRemove'];
+            	for (var i = 0; i < events.length; i++) {
+            	    $scope[events[i]] = function(e, editor) {
+            	        console.log('Event type:', e.type);
+            	    }
+            	};
+            	
+            	
+                if ('autofill' in attrs) {
+                    var nomeModelId = attrs.ngModel.split('.').pop();
+                    $scope.ngModel = $scope.$parent.data.entity.variabili[nomeModelId];
+                }   
+            }
+        };
+    }
+})();
