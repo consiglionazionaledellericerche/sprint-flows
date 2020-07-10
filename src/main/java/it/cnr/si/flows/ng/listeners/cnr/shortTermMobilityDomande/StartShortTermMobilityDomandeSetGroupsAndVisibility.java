1a75a3c6025d4f6352f7a82889349ba19ff66009
mtrycz <marcinireneusz.trycz@cnr.it>
2020-07-10 10:08:02 +0200

    Il case delle denominazioni non importa ai fini della comparazione

--- src/main/java/it/cnr/si/flows/ng/listeners/cnr/shortTermMobilityDomande/StartShortTermMobilityDomandeSetGroupsAndVisibility.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/cnr/shortTermMobilityDomande/StartShortTermMobilityDomandeSetGroupsAndVisibility.java
@@ -118,7 +118,7 @@ public class StartShortTermMobilityDomandeSetGroupsAndVisibility {
 			denominazioneEODirettore = direttoreAce.getDenominazioneEO();
 			if (!execution.getVariable("istitutoProponente").toString().equals("SEDE CENTRALE - DIPARTIMENTO")){
 				denominazioneEOProponente = execution.getVariable("istitutoProponente").toString().substring(9);
-				if (!denominazioneEODirettore.equals(denominazioneEOProponente)) {
+				if (!denominazioneEODirettore.equalsIgnoreCase(denominazioneEOProponente)) {
 					throw new BpmnError("400", "La struttura dichiarata dall'utente: " + userNameProponente + ": <br>" 
 							+ denominazioneEOProponente
 							+ "<br>non coincide con quella di afferenza amministrativa"
