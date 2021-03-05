1e81eedf2461f376c77106887f62468299b20114
Paolo Enrico Cirone <paoloenricocirone@iasi.cnr.it>
2021-03-05 10:24:38 +0100

    Pagine "i miei compiti" e "ricerca":  - beautify Code  - chiusura issue SonarLint

--- src/main/webapp/app/pages/search/search.controller.js
+++ src/main/webapp/app/pages/search/search.controller.js
@@ -1,92 +1,92 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module("sprintApp").controller("SearchController", SearchController);
+    angular.module("sprintApp").controller("SearchController", SearchController);
 
-  SearchController.$inject = ["$scope", "dataService", "utils", "$log", "$location"];
+    SearchController.$inject = ["$scope", "dataService", "utils", "$log", "$location"];
 
-  function SearchController($scope, dataService, utils, $log, $location) {
-    var vm = this,
-      oldUrl = $scope.formUrl;
-
-
-    $scope.reload = false;
-    // "conservo" i parametri della ricerca  ...
-    vm.searchParams = $location.search();
-    //parso bene i searchParams che sono date
-    if(vm.searchParams.hasOwnProperty('startDateLess'))
-      vm.searchParams.startDateLess = new Date(vm.searchParams.startDateLess);
-    if(vm.searchParams.hasOwnProperty('startDateGreat'))
-      vm.searchParams.startDateGreat = new Date(vm.searchParams.startDateGreat);
-
-    vm.searchParams.active = $location.search().active || true;
-    vm.searchParams.order = $location.search().order || "ASC";
-    if(!vm.searchParams.page)
-      vm.page = vm.searchParams.page = $location.search().page || 1;
-    vm.searchParams.processDefinitionKey = $location.search().processDefinitionKey || "all";
-
-    $scope.search = function() {
-      //serve per evitare di ricaricare le form di ricerca associate alla Process Definition ad ogni nuova ricerca
-      $scope.reload = $scope.formUrl !== oldUrl;
-
-      vm.results = [];
-      vm.loading = true;
-
-      if (vm.searchParams.processDefinitionKey === null)
-        vm.searchParams.processDefinitionKey = undefined;
-
-      $log.info(vm.searchParams);
-
-      $location.search(vm.searchParams);
-
-      //ripulisco i valori con null(tipo quando annullo la data selezionata)
-      Object.keys(vm.searchParams).forEach(function(key) {
-          if(vm.searchParams[key] == null)
-            delete vm.searchParams[key];
-      });
-      dataService.processInstances.search(vm.searchParams).then(
-        function(response) {
-          vm.results = utils.refactoringVariables(response.data.data);
-
-          vm.totalItems = response.data.total;
-          vm.loading = false;
-        },
-        function(response) {
-          $log.error(response);
-          vm.loading = false;
-        }
-      );
-    };
+    function SearchController($scope, dataService, utils, $log, $location) {
+        var vm = this,
+            oldUrl = $scope.formUrl;
 
 
-    $scope.exportCsv = function() {
-      dataService.search
-        .exportCsv(vm.searchParams, -1, -1)
-        .success(function(response) {
-          var filename = new Date().toISOString().slice(0, 10) + ".xls",
-            file = new Blob([response], {
-              type: "application/vnd.ms-excel"
+        $scope.reload = false;
+        // "conservo" i parametri della ricerca  ...
+        vm.searchParams = $location.search();
+        //parso bene i searchParams che sono date
+        if (vm.searchParams.hasOwnProperty('startDateLess'))
+            vm.searchParams.startDateLess = new Date(vm.searchParams.startDateLess);
+        if (vm.searchParams.hasOwnProperty('startDateGreat'))
+            vm.searchParams.startDateGreat = new Date(vm.searchParams.startDateGreat);
+
+        vm.searchParams.active = $location.search().active || true;
+        vm.searchParams.order = $location.search().order || "ASC";
+        if (!vm.searchParams.page)
+            vm.page = vm.searchParams.page = $location.search().page || 1;
+        vm.searchParams.processDefinitionKey = $location.search().processDefinitionKey || "all";
+
+        $scope.search = function() {
+            //serve per evitare di ricaricare le form di ricerca associate alla Process Definition ad ogni nuova ricerca
+            $scope.reload = $scope.formUrl !== oldUrl;
+
+            vm.results = [];
+            vm.loading = true;
+
+            if (vm.searchParams.processDefinitionKey === null)
+                vm.searchParams.processDefinitionKey = undefined;
+
+            $log.info(vm.searchParams);
+
+            $location.search(vm.searchParams);
+
+            //ripulisco i valori con null(tipo quando annullo la data selezionata)
+            Object.keys(vm.searchParams).forEach(function(key) {
+                if (vm.searchParams[key] == null)
+                    delete vm.searchParams[key];
             });
-          $log.info(file, filename);
-          saveAs(file, filename);
-        });
-    };
+            dataService.processInstances.search(vm.searchParams).then(
+                function(response) {
+                    vm.results = utils.refactoringVariables(response.data.data);
 
-    $scope.$watchGroup(
-      ["vm.searchParams.processDefinitionKey"],
-      function() {
-        if (vm.searchParams.processDefinitionKey) {
-            $scope.formUrl =
-              "api/forms/" +
-              vm.searchParams.processDefinitionKey +
-              "/1/search-pi";
-        } else {
-          $scope.formUrl = "api/forms/all/1/search-pi";
-        }
-      }
-    );
+                    vm.totalItems = response.data.total;
+                    vm.loading = false;
+                },
+                function(response) {
+                    $log.error(response);
+                    vm.loading = false;
+                }
+            );
+        };
 
 
-    $scope.search();
-  }
+        $scope.exportCsv = function() {
+            dataService.search
+                .exportCsv(vm.searchParams, -1, -1)
+                .success(function(response) {
+                    var filename = new Date().toISOString().slice(0, 10) + ".xls",
+                        file = new Blob([response], {
+                            type: "application/vnd.ms-excel"
+                        });
+                    $log.info(file, filename);
+                    saveAs(file, filename);
+                });
+        };
+
+        $scope.$watchGroup(
+            ["vm.searchParams.processDefinitionKey"],
+            function() {
+                if (vm.searchParams.processDefinitionKey) {
+                    $scope.formUrl =
+                        "api/forms/" +
+                        vm.searchParams.processDefinitionKey +
+                        "/1/search-pi";
+                } else {
+                    $scope.formUrl = "api/forms/all/1/search-pi";
+                }
+            }
+        );
+
+
+        $scope.search();
+    }
 })();
