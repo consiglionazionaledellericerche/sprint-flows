bfa06b6b440ebf80944fef900aa929543bfe59d8
massimo.fraticelli <massimo.fraticelli@cnr.it>
2021-09-03 10:31:08 +0200

    verifico la data della rendicontazione

--- src/main/java/it/cnr/si/flows/ng/listeners/cnr/covid19/StartCovid19SetGroupsAndVisibility_v1.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/cnr/covid19/StartCovid19SetGroupsAndVisibility_v1.java
@@ -71,7 +71,8 @@ public class StartCovid19SetGroupsAndVisibility_v1 {
 
 		} catch ( FeignException  e) {
 			if ((e.getMessage().indexOf("PERSONA_ASSEGNATA_SEDE_ESTERNA") >= 0)  && execution.getVariable("tipoAttivita").toString().equals("rendicontazione") ) {
-				responsabileStruttura = aceService.findResponsabileStruttura(initiator, LocalDate.now().minusMonths(1), TipoAppartenenza.SEDE, "responsabile-struttura");
+				dateRif = LocalDate.of(Integer.parseInt(execution.getVariable("anno").toString()), Integer.parseInt(execution.getVariable("meseNumerico").toString()), 1);
+				responsabileStruttura = aceService.findResponsabileStruttura(initiator, dateRif, TipoAppartenenza.SEDE, "responsabile-struttura");
 			} else {
 				throw e;
 			}
