2882dcdd52725522182703947f7dd1349c455497
massimo.fraticelli <massimo.fraticelli@cnr.it>
2021-11-15 16:18:00 +0100

    Primo codice per Attestati NEw

---
+++ src/main/java/it/cnr/si/flows/ng/listeners/cnr/attestati/ManageProcessAttestati_v1.java
@@ -0,0 +1,147 @@
+package it.cnr.si.flows.ng.listeners.cnr.attestati;
+
+
+
+import it.cnr.si.flows.ng.utils.Utils;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+import org.activiti.engine.delegate.Expression;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import it.cnr.si.flows.ng.service.FirmaDocumentoService;
+import it.cnr.si.flows.ng.service.FlowsProcessInstanceService;
+import it.cnr.si.flows.ng.utils.Enum;
+import it.cnr.si.flows.ng.utils.Enum.StatoAttestatiEnum;
+import it.cnr.si.flows.ng.utils.Enum.TipologieeMissioniEnum;
+import it.cnr.si.flows.ng.utils.SecurityUtils;
+import it.cnr.si.service.AceService;
+import it.cnr.si.service.ExternalMessageService;
+import it.cnr.si.service.dto.anagrafica.simpleweb.SimpleUtenteWebDto;
+import it.cnr.si.domain.enumeration.ExternalApplication;
+import it.cnr.si.domain.enumeration.ExternalMessageVerb;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
+
+@Component
+@Profile("cnr")
+public class ManageProcessAttestati_v1 implements ExecutionListener {
+	private static final long serialVersionUID = 686169707042367215L;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ManageProcessAttestati_v1.class);
+
+
+	@Value("${cnr.attestati.url}")
+	private String urlAttestati;
+	@Value("${cnr.missioni.attestatiPath}")
+	private String pathAttestati;
+
+
+	@Inject
+	private StartAttestatiSetGroupsAndVisibility startAttestatiSetGroupsAndVisibility;
+	@Inject
+	private ExternalMessageService externalMessageService;	
+	@Inject
+	private AceService aceService;
+	@Inject
+	private Utils utils;
+
+
+	private Expression faseEsecuzione;
+
+	public void restToApplicazioneAttestati(DelegateExecution execution, StatoAttestatiEnum statoAttestato, String user) {
+
+
+		String idStruttura = execution.getVariable("idStruttura").toString();;
+		String codiceSedeAttestato = execution.getVariable("codiceSedeAttestato").toString();
+		String meseAttestato = execution.getVariable("meseAttestato").toString();;
+		String annoAttestato = execution.getVariable("annoAttestato").toString();;
+		String tipologiaMissione = execution.getVariable("tipologiaMissione").toString();
+
+		String idSedeAttestatoFinal = idSedeAttestato;
+		Map<String, Object> missioniPayload = new HashMap<String, Object>()
+		{
+			{
+				put("tipologiaMissione", tipologiaMissione);
+				put("codiceSedeAttestato", codiceSedeAttestato);
+				put("stato", statoAttestato.name().toString());
+				put("processInstanceId", execution.getProcessInstanceId().toString());
+				put("user", user);
+				if(execution.getVariable("commento") != null) {
+					put("commento", execution.getVariable("commento").toString());
+				} else {
+					put("commento", "");
+				}
+			}	
+		};
+
+		String url = urlAttestati + pathAttestati;
+		externalMessageService.createExternalMessage(url, ExternalMessageVerb.POST, missioniPayload, ExternalApplication.ATTESTATI);
+	}
+
+
+	@Override
+	public void notify(DelegateExecution execution) throws Exception {
+		String currentUser = SecurityUtils.getCurrentUserLogin();
+		String processInstanceId =  execution.getProcessInstanceId();
+		String executionId =  execution.getId();
+		String stato =  execution.getCurrentActivityName();
+		String sceltaUtente = "start";
+		if(execution.getVariable("sceltaUtente") != null) {
+			sceltaUtente =  (String) execution.getVariable("sceltaUtente");	
+		}
+
+		LOGGER.info("ProcessInstanceId: " + processInstanceId);
+		String faseEsecuzioneValue = "noValue";
+		faseEsecuzioneValue = faseEsecuzione.getValue(execution).toString();
+		LOGGER.info("-- azioneScelta: " + faseEsecuzioneValue + " con sceltaUtente: " + sceltaUtente);
+
+		switch(faseEsecuzioneValue){  
+		// START
+		case "process-start": {
+			startAttestatiSetGroupsAndVisibility.configuraVariabiliStart(execution);
+		};break;    
+
+		// START
+		case "respinto-start": {
+			utils.updateJsonSearchTerms(executionId, processInstanceId, Enum.StatoAttestatiEnum.RESPINTO.toString());
+			restToApplicazioneAttestati(execution, Enum.StatoAttestatiEnum.RESPINTO, currentUser);		
+		};break;
+		
+		//case "respinto-end": {
+		//case "validazione-start": {
+		//case "validazione-end": {
+		
+
+		case "endevent-annulla": {
+			execution.setVariable("STATO_FINALE_DOMANDA", Enum.StatoAttestatiEnum.ANNULLATO);
+			execution.setVariable("statoFinale", Enum.StatoAttestatiEnum.ANNULLATO.toString());
+			utils.updateJsonSearchTerms(executionId, processInstanceId, Enum.StatoAttestatiEnum.ANNULLATO.toString());
+			restToApplicazioneAttestati(execution, Enum.StatoAttestatiEnum.ANNULLATO, currentUser);
+		};break;    	
+
+
+		case "endevent-approvato-start": {
+			execution.setVariable("STATO_FINALE_DOMANDA", Enum.StatoAttestatiEnum.APPROVATO);
+			execution.setVariable("statoFinale", Enum.StatoAttestatiEnum.APPROVATO.toString());
+			utils.updateJsonSearchTerms(executionId, processInstanceId, Enum.StatoAttestatiEnum.APPROVATO.toString());
+			restToApplicazioneAttestati(execution, Enum.StatoAttestatiEnum.APPROVATO, currentUser);
+		};break;  
+
+		case "process-end": {
+			//sbloccaDomandeBando(execution);
+		};break; 
+		// DEFAULT  
+		default: {
+		};break;
+
+		} 
+	}
+
+
+}
---
+++ src/main/java/it/cnr/si/flows/ng/listeners/cnr/attestati/StartAttestatiSetGroupsAndVisibility.java
@@ -0,0 +1,66 @@
+package it.cnr.si.flows.ng.listeners.cnr.attestati;
+
+
+
+import it.cnr.si.flows.ng.repository.SetTimerDuedateCmd;
+import it.cnr.si.flows.ng.service.FlowsProcessInstanceService;
+import it.cnr.si.flows.ng.service.FlowsTimerService;
+import it.cnr.si.flows.ng.utils.Enum;
+import it.cnr.si.service.AceService;
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.ManagementService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.runtime.Job;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static it.cnr.si.flows.ng.utils.Utils.PROCESS_VISUALIZER;
+
+
+
+@Component
+@Profile("cnr")
+
+@Service
+public class StartAttestatiSetGroupsAndVisibility {
+	private static final Logger LOGGER = LoggerFactory.getLogger(StartAttestatiSetGroupsAndVisibility.class);
+
+	@Inject
+	private RuntimeService runtimeService;
+	@Inject
+	private AceService aceService;
+
+	public void configuraVariabiliStart(DelegateExecution execution)  throws IOException, ParseException  {
+
+		String initiator = (String) execution.getVariable(Enum.VariableEnum.initiator.name());
+		int idStruttura = Integer.parseInt(execution.getVariable("idStruttura").toString());
+		String utenteRichiedente = execution.getVariable("utenteRichiedente").toString();
+		String meseAttestato = execution.getVariable("meseAttestato").toString();
+		String annoAttestato = execution.getVariable("annoAttestato").toString();
+		String codiceSedeAttestato = aceService.entitaOrganizzativaById(idStruttura).getIdnsip();
+		String codiceCdsuoAttestato = aceService.entitaOrganizzativaById(idStruttura).getCdsuo();
+
+		// LOGGER.info("L'utente {} sta avviando il flusso {} (con titolo {})", initiator, execution.getId(), execution.getVariable(Enum.VariableEnum.title.name()));
+		LOGGER.info("L'utente {} sta avviando il flusso {} (con titolo {} per l'utente {} - mese:  {} - anno  {})", initiator, execution.getId(), execution.getVariable("title"), utenteRichiedente, meseAttestato, annoAttestato );
+
+		String gruppoValidatoriAttestati = "validatoreAttestati@" + idStruttura;
+
+		LOGGER.debug("Imposto i gruppi del flusso: gruppoValidatoriAttestati {} ",  gruppoValidatoriAttestati);
+		execution.setVariable("gruppoValidatoriAttestati", gruppoValidatoriAttestati);
+		execution.setVariable("codiceSedeAttestato", codiceSedeAttestato);
+		execution.setVariable("codiceCdsuoAttestato", codiceCdsuoAttestato);
+
+	}
+}
