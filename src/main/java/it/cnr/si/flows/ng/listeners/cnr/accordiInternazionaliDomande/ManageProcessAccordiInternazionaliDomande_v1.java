d14ac3bfcad965908789f137a331c52e320a9448
mtrycz <marcinireneusz.trycz@cnr.it>
2022-06-17 09:34:51 +0200

    typo

--- src/main/java/it/cnr/si/flows/ng/listeners/cnr/accordiInternazionaliDomande/ManageProcessAccordiInternazionaliDomande_v1.java
+++ src/main/java/it/cnr/si/flows/ng/listeners/cnr/accordiInternazionaliDomande/ManageProcessAccordiInternazionaliDomande_v1.java
@@ -119,7 +119,7 @@ public class ManageProcessAccordiInternazionaliDomande_v1 implements ExecutionLi
 			case "process-start": {
 			    String propostaHtml = String.valueOf(execution.getVariable("proposta_di_ricerca"));
 			    String propostaPulita = Utils.sanitizeHtml(propostaHtml);
-			    execution.setVariable("pproposta_di_ricerca", propostaPulita);
+			    execution.setVariable("proposta_di_ricerca", propostaPulita);
 			    
 			    execution.setVariable(null, faseEsecuzioneValue);
 			    
