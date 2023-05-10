d5436f288a2b165a2d054e9c7e65ec285e73305a
mtrycz <marcinireneusz.trycz@cnr.it>
2023-05-10 12:40:15 +0200

    sso funziona con oauth/token

--- src/main/java/it/cnr/si/web/rest/DeprecatedOauthResource.java
+++ src/main/java/it/cnr/si/web/rest/DeprecatedOauthResource.java
@@ -3,7 +3,6 @@ package it.cnr.si.web.rest;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Base64;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -16,6 +15,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,39 +29,36 @@ public class DeprecatedOauthResource {
     private String defaultClientSecret;
     @Value("${keycloak.auth-server-url}")
     private String ssoUrl;
+    @Value("${keycloak.realm}")
+    private String ssoRealm;
     
     @Deprecated
     @PostMapping(
             path = "oauth/token",
             consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE},
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> autorizeoauth(
-            @RequestHeader HttpHeaders inHeaders) throws URISyntaxException {
-        
+        public ResponseEntity<String> autorizeoauth(
+                @RequestHeader HttpHeaders inHeaders,
+                @RequestParam String grant_type,
+                @RequestParam(required = false) String client_id,
+                @RequestParam(required = false) String client_secret,
+                @RequestParam(required = false) String username,
+                @RequestParam(required = false) String password) throws URISyntaxException {
+            
         RestTemplate rt = new RestTemplate();
         
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        
-        String client_id = null, client_secret = null;
-        
-        List<String> auths = inHeaders.get("Authorization");
-        if (auths.size() > 0) {
-            String auth = auths.get(0);
-            if (auth.startsWith("Basic ")) {
-                auth = new String(Base64.getDecoder().decode(auth.substring(6)));
-                String[] creds = auth.split(":");
-                client_id = creds[0];
-                client_secret = creds[1];
-            }
-        }
+        String toEncode = defaultClientId+":"+defaultClientSecret;
+        String basicAuth = Base64.getEncoder().encodeToString(toEncode.getBytes());
+        headers.set("authorization", "Basic "+basicAuth);
         
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("client_id", client_id);
-        params.add("client_secret", client_secret);
-        params.add("grant_type", "client_credentials");
+        params.add("username", username);
+        params.add("password", password);
+        params.add("grant_type", "password");
         
-        URI url = new URI(ssoUrl+"/realms/cnr/protocol/openid-connect/token");
+        URI url = new URI(ssoUrl+"/realms/"+ ssoRealm +"/protocol/openid-connect/token");
         RequestEntity<MultiValueMap<String, String>> request = 
                 new RequestEntity<MultiValueMap<String, String>>(params, headers, HttpMethod.POST, url);
         
