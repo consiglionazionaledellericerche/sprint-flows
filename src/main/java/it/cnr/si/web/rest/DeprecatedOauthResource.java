94e6b91de5fa50d8ddb6419473086b111626dcc1
mtrycz <marcinireneusz.trycz@cnr.it>
2023-05-15 10:54:46 +0200

    Inoltra il token solo col profilo keycloak (senno' non logga da UI)

--- src/main/java/it/cnr/si/web/rest/DeprecatedOauthResource.java
+++ src/main/java/it/cnr/si/web/rest/DeprecatedOauthResource.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
@@ -21,6 +22,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/")
+@Profile("keycloak")
 public class DeprecatedOauthResource {
 
     @Value("${cnr.ssoClientId}")
