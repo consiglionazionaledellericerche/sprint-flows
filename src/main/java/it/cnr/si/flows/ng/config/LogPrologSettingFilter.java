018d62f14e869ae34104f22c929dcba78fb4b48d
mtrycz <marcinireneusz.trycz@cnr.it>
2023-09-28 12:22:50 +0200

    Riaggiunto log di username e impersonator

--- src/main/java/it/cnr/si/flows/ng/config/LogPrologSettingFilter.java
+++ src/main/java/it/cnr/si/flows/ng/config/LogPrologSettingFilter.java
@@ -1,19 +1,29 @@
 package it.cnr.si.flows.ng.config;
 
 
-import it.cnr.si.flows.ng.utils.SecurityUtils;
-import it.cnr.si.service.SecurityService;
-
-import org.slf4j.MDC;
-import org.springframework.web.filter.GenericFilterBean;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.Optional;
 
 import javax.inject.Inject;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import java.io.IOException;
+import javax.validation.constraints.Email;
 
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.slf4j.MDC;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import it.cnr.si.flows.ng.utils.SecurityUtils;
+import it.cnr.si.service.SecurityService;
+
+@Component
 public class LogPrologSettingFilter extends GenericFilterBean {
 
     @Inject
@@ -24,13 +34,33 @@ public class LogPrologSettingFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
-            if (!securityUtils.getCurrentUserAuthorities().contains("PREVIOUS_ADMINISTRATOR"))
-                MDC.put("currentUser", securityService.getCurrentUserLogin());
-            else
-                MDC.put("currentUser", "admin as " + securityService.getCurrentUserLogin());
+            if (securityService.isAuthenticated()) {
+                if (getImpersonatorName().isPresent())
+                    MDC.put("currentUser", getImpersonatorName().get() + " as " + securityService.getCurrentUserLogin());
+                else 
+                    MDC.put("currentUser", securityService.getCurrentUserLogin());
+            } else {
+                MDC.put("currentUser", "not logged in");
+            }
         } catch (Exception e) {
             MDC.put("currentUser", "undefined");
         }
         chain.doFilter(request, response);
     }
+    
+    private Optional<String> getImpersonatorName() {
+        try {
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (principal instanceof KeycloakPrincipal<?>) {
+                KeycloakPrincipal<KeycloakSecurityContext> keycloakPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) principal;
+                Object impersonator = keycloakPrincipal.getKeycloakSecurityContext().getToken().getOtherClaims().get("impersonator");
+                if (impersonator != null) {
+                    LinkedHashMap<String, String> i = (LinkedHashMap<String, String>)impersonator;
+                    return Optional.of(String.valueOf(i.get("username")));
+                }
+            }
+        } catch (Exception e) {}
+        return Optional.empty();
+    }
+
 }
