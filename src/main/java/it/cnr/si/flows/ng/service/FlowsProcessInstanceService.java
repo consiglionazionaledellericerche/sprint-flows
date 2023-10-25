42687474d7ab2cd263f6e866945b9fd4fadc0dd1
TRYCZ MARCINIRENEUSZ <marcinireneusz.trycz@cnr.it>
2023-10-25 16:12:36 +0200

    Revert "Flow detail response times halved"
    This reverts commit f89fb8762a12e4a82aeb26323afcab10884f1130

--- src/main/java/it/cnr/si/flows/ng/service/FlowsProcessInstanceService.java
+++ src/main/java/it/cnr/si/flows/ng/service/FlowsProcessInstanceService.java
@@ -132,12 +132,12 @@ public class FlowsProcessInstanceService {
             List<Map<String, Object>> linkedFlows = new ArrayList<>();
 
             for (String linkedProcessId : values) {
-                HistoricProcessInstance linkedProcessInstance = historyService
-                        .createHistoricProcessInstanceQuery()
-                        .processInstanceId(linkedProcessId)
-                        .includeProcessVariables()
-                        .singleResult();
-                if(permissionEvaluator.canVisualize(linkedProcessId, linkedProcessInstance)) {
+                if(permissionEvaluator.canVisualize(linkedProcessId)) {
+                    HistoricProcessInstance linkedProcessInstance = historyService
+                            .createHistoricProcessInstanceQuery()
+                            .processInstanceId(linkedProcessId)
+                            .includeProcessVariables()
+                            .singleResult();
 
                     if (linkedProcessInstance != null) {
                         String key = linkedProcessInstance.getBusinessKey();
@@ -166,16 +166,22 @@ public class FlowsProcessInstanceService {
         result.put("identityLinks", identityLinks);
 
         // permessi aggiuntivi
-        result.put("canPublish", permissionEvaluator.canPublishAttachment(processInstanceId, processInstance));
-        result.put("canUpdateAttachments", permissionEvaluator.canUpdateAttachment(processInstanceId, processInstance));
-        result.put("isRevocabile", isRevocabile(processInstanceId, processInstance));
+        result.put("canPublish", permissionEvaluator.canPublishAttachment(processInstanceId));
+        result.put("canUpdateAttachments", permissionEvaluator.canUpdateAttachment(processInstanceId));
+        result.put("isRevocabile", isRevocabile(processInstanceId));
         
         if(whitTaskList){
-            result.put("history", getHistoryForPi(processInstanceId, processInstance));
+            result.put("history", getHistoryForPi(processInstanceId));
         }
         return result;
     }
-    public ArrayList<Map<String, Object>> getHistoryForPi(String processInstanceId, HistoricProcessInstance processInstance){
+
+
+    public ArrayList<Map<String, Object>> getHistoryForPi(String processInstanceId){
+        // PrecessInstance metadata
+        HistoricProcessInstance processInstance = historyService.createHistoricProcessInstanceQuery()
+                .processInstanceId(processInstanceId)
+                .singleResult();
 
         final Map<String, Object> identityLinks = new LinkedHashMap<>();
 
@@ -230,14 +236,6 @@ public class FlowsProcessInstanceService {
                         });
 
         return history;
-    }
-
-    public ArrayList<Map<String, Object>> getHistoryForPi(String processInstanceId){
-        // PrecessInstance metadata
-        HistoricProcessInstance processInstance = historyService.createHistoricProcessInstanceQuery()
-                .processInstanceId(processInstanceId)
-                .singleResult();
-        return getHistoryForPi(processInstanceId, processInstance);
     }
 
 
@@ -486,17 +484,22 @@ public class FlowsProcessInstanceService {
      * 3. l'utente loggato e' il boss del richiedente
      * 4. la domanda non e' stata gia' revocata
      */
-    public boolean isRevocabile(String processInstanceId, HistoricProcessInstance processInstance) {
-// 1. e' accettata (il flusso e' concluso)
+    public boolean isRevocabile(String processInstanceId) {
+        HistoricProcessInstance processInstance = historyService.createHistoricProcessInstanceQuery()
+                .processInstanceId(processInstanceId)
+                .includeProcessVariables()
+                .singleResult();
+        
+        // 1. e' accettata (il flusso e' concluso)
         if (processInstance.getEndTime() == null)
             return false;
         if (!"VALIDATA".equals(processInstance.getProcessVariables().get("statoFinaleDomanda")) && !"PRESA_VISIONE".equals(processInstance.getProcessVariables().get("statoFinaleDomanda")))
             return false;
-
+        
         // 2. il flusso e' di tipo revocabile
         if (!processiRevocabili.containsKey(processInstance.getProcessDefinitionKey()))
             return false;
-
+        
         // 3. l'utente loggato e' abilitato alla Revoca
         String currentUser = securityService.getCurrentUserLogin();
         Set<String> allRolesForUser = membershipService.getAllRolesForUser(currentUser);
@@ -504,7 +507,7 @@ public class FlowsProcessInstanceService {
         if ( abilitatiAllaRevoca.get(processInstance.getProcessDefinitionKey()).stream()
                 .noneMatch(ruoloRevoca -> allRolesForUser.contains(ruoloRevoca + "@" + idAceStrutturaDomandaRichiedente)) )
             return false;
-
+        
         // 4. la domanda non e' stata gia' revocata e non è in corso di revoca
         List<HistoricProcessInstance> revoche = historyService.createHistoricProcessInstanceQuery()
                 .processDefinitionKey(processiRevocabili.get(processInstance.getProcessDefinitionKey()))
@@ -521,14 +524,6 @@ public class FlowsProcessInstanceService {
 
         // se nessuno dei controlli è fallito, il flusso è revocabile
         return true;
-    }
-
-    public boolean isRevocabile(String processInstanceId) {
-        HistoricProcessInstance processInstance = historyService.createHistoricProcessInstanceQuery()
-                .processInstanceId(processInstanceId)
-                .includeProcessVariables()
-                .singleResult();
-        return isRevocabile(processInstanceId, processInstance);
     }
 
     
--- src/main/java/it/cnr/si/security/PermissionEvaluatorImpl.java
+++ src/main/java/it/cnr/si/security/PermissionEvaluatorImpl.java
@@ -169,22 +169,18 @@ public class PermissionEvaluatorImpl implements PermissionEvaluator {
      * @param processInstanceId       Il process instance id
      * @return risultato della verifica dei permessi (booleano)
      */
-    public boolean canVisualize(String processInstanceId, HistoricProcessInstance historicProcessInstance) {
+    public boolean canVisualize(String processInstanceId) {
         String userName = securityService.getCurrentUserLogin();
         List<String> authorities = getAuthorities(userName);
 
-        return canVisualize((String) historicProcessInstance.getProcessVariables().get(ID_STRUTTURA),
-                historicProcessInstance.getProcessDefinitionKey(),
-                processInstanceId, authorities, userName);
-    }
-
-    public boolean canVisualize(String processInstanceId) {
         HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery()
                 .includeProcessVariables()
                 .processInstanceId(processInstanceId)
                 .singleResult();
 
-        return canVisualize(processInstanceId, historicProcessInstance);
+        return canVisualize((String) historicProcessInstance.getProcessVariables().get(ID_STRUTTURA),
+                            historicProcessInstance.getProcessDefinitionKey(),
+                            processInstanceId, authorities, userName);
     }
 
 
@@ -323,10 +319,16 @@ public class PermissionEvaluatorImpl implements PermissionEvaluator {
     }
 
 
-    public boolean canUpdateAttachment(String processInstanceId, HistoricProcessInstance instance) {
+
+    public boolean canUpdateAttachment(String processInstanceId) {
+
         if(securityService.isCurrentUserInRole("ROLE_ADMIN"))
             return true;
 
+        HistoricProcessInstance instance = historyService.createHistoricProcessInstanceQuery()
+                .processInstanceId(processInstanceId)
+                .includeProcessVariables()
+                .singleResult();
         boolean isActive = instance.getEndTime() == null;
 
         String processDefinitionKey = instance.getProcessDefinitionKey();
@@ -355,21 +357,15 @@ public class PermissionEvaluatorImpl implements PermissionEvaluator {
         return (isResponsabile || isRuoloFlusso) && isActive;
     }
 
+    public boolean canPublishAttachment(String processInstanceId) {
 
-    public boolean canUpdateAttachment(String processInstanceId) {
+        if(securityService.isCurrentUserInRole("ROLE_ADMIN"))
+            return true;
 
         HistoricProcessInstance instance = historyService.createHistoricProcessInstanceQuery()
                 .processInstanceId(processInstanceId)
                 .includeProcessVariables()
                 .singleResult();
-
-        return canUpdateAttachment(processInstanceId, instance);
-    }
-    public boolean canPublishAttachment(String processInstanceId, HistoricProcessInstance instance) {
-
-        if(securityService.isCurrentUserInRole("ROLE_ADMIN"))
-            return true;
-
         String username = securityService.getCurrentUserLogin();
 
         if (instance.getProcessDefinitionKey().equals(acquisti.getProcessDefinition())) {
@@ -385,15 +381,6 @@ public class PermissionEvaluatorImpl implements PermissionEvaluator {
         }
 
         return false;
-    }
-
-    public boolean canPublishAttachment(String processInstanceId) {
-
-        HistoricProcessInstance instance = historyService.createHistoricProcessInstanceQuery()
-                .processInstanceId(processInstanceId)
-                .includeProcessVariables()
-                .singleResult();
-        return canPublishAttachment(processInstanceId, instance);
     }
 
     @Override
