ddc9e27c289695aae879bfebf12e733332a7f075
CIRONE PAOLO ENRICO <paoloenrico.cirone@cnr.it>
2024-01-30 09:24:51 +0100

    lavoro di fix vulnerabilità con docker scout

--- src/main/docker/locale/run_sprint-flows_locale.sh
+++ src/main/docker/locale/run_sprint-flows_locale.sh
@@ -5,21 +5,22 @@ set -e
 
 RED='\033[0;31m'
 NC='\033[0m'
-#
-#echo -e "\n################################## Buildo il war #################################################\n"
-#cd ../../../../
-#mvn clean install -Pprod -DskipTests -X   #comando eseguito da Jenckins
-##mvn clean install -DskipTests
-#
-#echo -e "\n################################## Copio il war nella folder di docker #################################################\n"
-#cd src/main/docker/locale
+
+echo -e "\n################################## Buildo il war #################################################\n"
+cd ../../../../
+mvn clean install -Pprod -DskipTests   #comando eseguito da Jenckins
+#mvn clean install -DskipTests
+
+echo -e "\n################################## Copio il war nella folder di docker #################################################\n"
+cd src/main/docker/locale
 #cp ../../../../target/app.war app.war
 
 echo -e "\n################################## RIMUOVO sprint-flows-locale #################################################\n"
 docker rmi -f sprint-flows-locale
 
 echo -e "\n################################## BUILDING sprint-flows-locale #################################################\n"
-docker build --no-cache -t sprint-flows-locale -f Dockerfile .
+#docker build --no-cache -t sprint-flows-locale -f Dockerfile .
+docker build --no-cache -t sprint-flows-locale -f Dockerfile_amazonCorretto .
 
 
 echo -e "\n################################## RUN sprint-flows-locale #################################################\n"
